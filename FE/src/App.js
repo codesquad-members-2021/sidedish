3df11fc9cbe8d91784a2b7eb836b3dd5323c170e
Jaeuk-YOO <ryou9305@naver.com>
2021-04-19 22:21:05 +0900

    add: react app initialize

---
+++ FE/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+import MainPage from './Pages/MainPage';
+
+function App() {
+  return (
+    <MainPage />
+  );
+}
+
+export default App;
---
+++ FE/src/Pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div>
+      메인페이지입니다.
+    </div>
+  )
+}
+
+export default MainPage
