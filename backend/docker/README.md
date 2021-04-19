844e98c8714e91ec9d77be6412acde772e9ba48a
고정완 (Can) <jw.gho@ecubelabs.com>
2021-04-19 16:41:10 +0900

    [test] MySQL5.7 도커화

---
+++ backend/docker/README.md
@@ -0,0 +1,35 @@
+# Docker 안내서
+
+## Docker Clear
+
+```sh
+sh ./docker-clear.sh
+```
+
+## Docker Compose
+
+```sh
+cd ./sidedish/backend/docker
+
+# start
+docker-compose up --build
+docker-compose up -d
+
+# view logs
+docker-compose logs
+
+# stop
+docker-compose down
+```
+
+## 접속
+
+```sh
+mysql -h 127.0.0.1 -u root -p
+# 비밀번호: rootpw
+
+mysql -h 127.0.0.1 -u pyro -p
+# 비밀번호: pyropw
+```
+
+혹은 `http://localhost:3030` 에 브라우저로 접속
---
+++ backend/docker/db/initdb/2-insert.sql
@@ -0,0 +1,48 @@
+INSERT INTO sidedish.dish (
+    detail_hash,
+    image,
+    alt,
+    delivery_type,
+    title,
+    description,
+    n_price,
+    s_price,
+    badge,
+    created_date)
+VALUES
+(
+    "H9881",
+    "https://cdn.bmf.kr/_data/product/H9881/f2a7b4df359c850b1a9eb57e17ddf6fc.jpg",
+    "[소중한식사] 경상도 한상차림",
+    "[새벽배송, 전국택배]",
+    "[소중한식사] 경상도 한상차림",
+    "경상도 명물 요리 세 가지를 한 상에!",
+    39000,
+    31200,
+    "이벤트특가",
+    now()
+),
+(
+    "HDF4C",
+    "https://cdn.bmf.kr/_data/product/HDF4C/954b78f9111bbef54ede9fdcdf1298d8.jpg",
+    "[딩고] 시그니처 3종 SET",
+    "[새벽배송, 전국택배]",
+    "[딩고] 시그니처 3종 SET",
+    "오늘 저녁은 이상하게 안주빨을 세워보고 싶다!",
+    32800,
+    24000,
+    "이벤트특가",
+    now()
+),
+(
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    1,
+    2,
+    "",
+    now()
+);
