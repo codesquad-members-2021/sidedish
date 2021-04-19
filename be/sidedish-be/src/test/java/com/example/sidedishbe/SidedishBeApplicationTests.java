a72f475df6f9481a5ea38b7cdd54d67739d7b837
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-04-19 16:50:04 +0900

    [CHORE] : 프로젝트 초기화

---
+++ be/sidedish-be/src/test/java/com/example/sidedishbe/SidedishBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidedishbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidedishBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
