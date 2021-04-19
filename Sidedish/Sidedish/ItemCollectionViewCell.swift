6f8a424239af9ef59ac380345a3ca48640dea25b
lena <sue822330@gmail.com>
2021-04-19 15:23:59 +0900

    main 스토리보드에 collection view 추가

---
+++ Sidedish/Sidedish/ItemCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCollectionViewCell.swift
+//  Sidedish
+//
+//  Created by Ador on 2021/04/19.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ItemCollectionViewCell"
+    
+}
--- Sidedish/Sidedish/ViewController.swift
+++ Sidedish/Sidedish/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemCollectionViewCell.identifier, for: indexPath) as? ItemCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 130)
+    }
+    
+}
