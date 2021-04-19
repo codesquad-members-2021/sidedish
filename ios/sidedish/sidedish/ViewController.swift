de65da5ee2107fcfbae10982b2ad4242699ce8a8
jhpark-steve <park9368@naver.com>
2021-04-20 00:58:25 +0900

    style: 메인 화면 UI 구성 (#8)
    TableView와 기본 뼈대가 되는 Cell을 Storyboard에 추가

---
+++ ios/sidedish/sidedish/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  sidedish
+//
+//  Created by 박정하 on 2021/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
