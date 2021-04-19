5f8ac65dc6b9a047c855b4334fe54750d8ad2d6e
Raccoon <nobllizdc@gmail.com>
2021-04-19 23:31:34 +0900

    Fix. TagBox로 감싸서 display: flex 적용

--- raqqoon-sidedish/src/components/TabUI/TabUI.jsx
+++ raqqoon-sidedish/src/components/TabUI/TabUI.jsx
@@ -1,4 +1,5 @@
 import { EVENT, LAUNCHING } from 'const';
+import styled from 'styled-components';
 import Info from './Info';
 import Price from './Price';
 import Tag from './Tag';
@@ -9,11 +10,17 @@ const TabUI = () => {
       {/* data 들어갈 자리 null */}
       <Info name={null} body={null} />
       <Price normal={null} discount={`11,900원`} />
-      <Tag type={EVENT} />
-      <Tag type={LAUNCHING} />
-      <Tag />
+      <TagBoxDiv>
+        <Tag type={EVENT} />
+        <Tag type={LAUNCHING} />
+        <Tag />
+      </TagBoxDiv>
     </>
   );
 };
 
 export default TabUI;
+
+const TagBoxDiv = styled.div`
+  display: flex;
+`;
