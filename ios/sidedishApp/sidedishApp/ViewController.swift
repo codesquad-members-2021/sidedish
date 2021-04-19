fdb6b61c54095af52ea30a466fec84c1ccdf090d
zombietux <>
2021-04-19 17:18:28 +0900

    inital project & 불필요한 주석 삭제

---
+++ ios/sidedishApp/sidedishApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  sidedishApp
+//
+//  Created by zombietux on 2021/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
