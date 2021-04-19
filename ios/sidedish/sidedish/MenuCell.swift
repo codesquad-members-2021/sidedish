1ee08f0550f202f0da4dcbb5c5facaaa4fa7412b
jhpark-steve <park9368@naver.com>
2021-04-20 01:06:25 +0900

    feat: tableView의 customcell class 생성 (#8)
    label 업데이트 함수 작성. 후에 수정 필요

---
+++ ios/sidedish/sidedish/MenuCell.swift
@@ -0,0 +1,30 @@
+//
+//  MenuCell.swift
+//  sidedish
+//
+//  Created by 박정하 on 2021/04/19.
+//
+
+import UIKit
+
+class MenuCell : UITableViewCell {
+    @IBOutlet weak var menuImage: UIImageView!
+    
+    @IBOutlet weak var menuTitle: UILabel!
+    @IBOutlet weak var menuSubTitle: UILabel!
+    @IBOutlet weak var reducedPrice: UILabel!
+    @IBOutlet weak var menuPrice: UILabel!
+    @IBOutlet weak var specialPrice: UILabel!
+    
+    func updateMenu(titleText: String, subTitle: String, reducedPrice: String, price: String, isSpecialPrice: Bool){
+        self.menuTitle.text = titleText
+        self.menuSubTitle.text = subTitle
+        self.reducedPrice.text = reducedPrice
+        self.menuPrice.text = price
+        self.specialPrice.isHidden = isSpecialPrice
+        
+        if isSpecialPrice {
+            menuPrice.isHidden = true
+        }
+    }
+}
