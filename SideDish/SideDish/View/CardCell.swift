c220daa69e97498da4d742a3a87e3b0c59e36565
zeke <yangsin1031@naver.com>
2021-04-19 17:04:56 +0900

    feat: add collectionview cell

---
+++ SideDish/SideDish/View/CardCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCell.swift
+//  SideDish
+//
+//  Created by 양준혁 on 2021/04/19.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
