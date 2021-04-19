efa3cb637f25022572bee277193ac39e7ab127f3
younggil <zelord.kwoun@gmail.com>
2021-04-19 17:22:08 +0900

    [ADD] App.js Structure design

--- fe/sidedish-fe/src/App.js
+++ fe/sidedish-fe/src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>네이스행님 잘부탁드림다</h1>
+      <div className="Header">
+        <h1>BANCHAN 타이틀</h1>
+        <div>Header Title</div>
+        <div>Header Link</div>
+        <div>SearchBox</div>
+        <div>Login</div>
+        <div>Cart</div>
+      </div>
+      <div className="BestMenu">
+        <h1>후기가 증명하는 베스트 반찬</h1>
+        <div></div>
+      </div>
+      <div className="MenuSlider">
+        <h1>모두가 좋아하는 든든한 메인요리</h1>
+        <div></div>
+      </div>
+      <div className="Category">
+        <h1>모든 카테고리보기</h1>
+      </div>
     </div>
   );
 }
