65769c30b86f22e63bcd19be36c39eccd3e8ffc4
Sonjh1306 <yale1306@gmail.com>
2021-04-19 16:58:43 +0900

    build: create initial project (#1)

---
+++ frontend/SideDish/SideDish/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  SideDish
+//
+//  Created by sonjuhyeong on 2021/04/19.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
---
+++ frontend/SideDish/SideDish/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SideDish
+//
+//  Created by sonjuhyeong on 2021/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
