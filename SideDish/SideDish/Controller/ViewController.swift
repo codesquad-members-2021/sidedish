e60becb93d7928cef7d54e9f813800faa685e5d5
ellyheetov <yaaajcm@naver.com>
2021-04-19 21:53:36 +0900

    feat: add header in collection view

--- SideDish/SideDish/Controller/ViewController.swift
+++ SideDish/SideDish/Controller/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var cardCollectionView: UICollectionView!
     
+    @IBOutlet weak var cardCollectionView: UICollectionView!
 
+    var sectionHeaderTitle : [String] = ["모두가 좋아하는 든든한 메인요리", "정성이 담긴 뜨끈뜨끈 국물요리", "식탁을 풍성하게 하는 정갈한 밑반찬"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.cardCollectionView.dataSource = self
@@ -22,11 +23,14 @@ class ViewController: UIViewController {
         let nibName = UINib(nibName: "CardCell", bundle: nil)
         cardCollectionView.register(nibName, forCellWithReuseIdentifier: "cardCell")
     }
-
+    
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 3
     }
     
@@ -34,7 +38,27 @@ extension ViewController: UICollectionViewDataSource {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardCell", for: indexPath) as! CardCell
         return cell
     }
-    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard
+                let headerView = collectionView.dequeueReusableSupplementaryView(
+                    ofKind: kind,
+                    withReuseIdentifier: "\(HeaderView.self)",
+                    for: indexPath) as? HeaderView
+            else {
+                fatalError("Invalid view type")
+            }
+            
+            let searchTerm = sectionHeaderTitle[indexPath.section]
+            headerView.label.text = searchTerm
+            headerView.label.font = .boldSystemFont(ofSize: 22)
+            return headerView
+        default:
+            assert(false, "Invalid element type")
+        }
+    }
     
 }
 
