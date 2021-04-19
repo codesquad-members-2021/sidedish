f09a5583e7303147380e4bf412d3c6ac02953e4b
bong6981 <kbk5190@naver.com>
2021-04-19 16:37:38 +0900

    feat: Initial Commit
    - 의존성 추가
    
    issue: #2

---
+++ BE/src/main/java/com/team10/banchan/BanchanApplication.java
@@ -0,0 +1,13 @@
+package com.team10.banchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanchanApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/team10/banchan/BanchanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team10.banchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanchanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
