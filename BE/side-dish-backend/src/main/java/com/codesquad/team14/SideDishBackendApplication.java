07234b2c5d4163bec45e261b4fb69d3a0708c27c
Sally Oh <min27604@hotmail.com>
2021-04-19 21:12:34 +0900

    chore(init): 프로젝트 초기화 설정

---
+++ BE/side-dish-backend/src/main/java/com/codesquad/team14/SideDishBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codesquad.team14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideDishBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideDishBackendApplication.class, args);
+    }
+
+}
---
+++ BE/side-dish-backend/src/test/java/com/codesquad/team14/SideDishBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.team14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideDishBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
