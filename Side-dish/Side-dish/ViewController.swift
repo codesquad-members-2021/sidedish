fffce187ea69d48d7dcfb569f6824595a1e5b4ff
hoonha choi <sosqsz2@naver.com>
2021-04-19 14:14:16 +0900

    feat : 프로젝트 생성

---
+++ Side-dish/Side-dish/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Side-dish
+//
+//  Created by HOONHA CHOI on 2021/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
