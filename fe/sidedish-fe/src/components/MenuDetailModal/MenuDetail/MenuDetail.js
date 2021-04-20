1b04315a66593de46a58de855e0c429fbec67d7e
cchoongh <cndgh49@naver.com>
2021-04-20 14:05:44 +0900

    [ADD]: 모달 초기 구현
    * common/Modal 컴포넌트 추가. content 컴포넌트를 자식으로 둔 다음. content가 가운데 배치되도록 구현
    * MenuDetailModal에서 common/Modal을 사용하고, content로 MenuDetail 컴포넌트를 사용하도록 구조를 짜봤음.

---
+++ fe/sidedish-fe/src/components/MenuDetailModal/MenuDetail/MenuDetail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledMenuDetail = styled.div`
+  width: 500px;
+  height: 550px;
+  background-color: #FFFFFF;
+`;
+
+function MenuDetail() {
+  return (
+    <StyledMenuDetail>
+    </StyledMenuDetail>
+  );
+};
+
+export default MenuDetail;
---
+++ fe/sidedish-fe/src/components/common/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Modal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  pointer-events: none;
+`;
+
+export default Modal;
