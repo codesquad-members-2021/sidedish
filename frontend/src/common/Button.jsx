955ab0673965c011712f60f25fcc79fa5027eda1
eve712 <62237639+eve712@users.noreply.github.com>
2021-04-20 17:12:34 +0900

    feat: Button 컴포넌트 생성

---
+++ frontend/src/common/Button.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+const BtnStyle = styled.button`
+    display: block;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    width: 440px;
+    height: 58px;
+    padding: 16px;
+    box-shadow: 0px 0px 4px rgba(204, 204, 204, 0.5), 0px 2px 4px rgba(0, 0, 0, 0.25);
+    background: ${({type}) => type === 'order' ? '#82D32D' : '#E0E0E0'};
+    span {
+        color: ${({type}) => type === 'order' ? '#fff' : '#bdbdbd'};
+        font-size: 18px;
+        font-weight: bold;
+    }
+`
+
+const OrderBtn = ({type}) => {
+    return (
+        <BtnStyle type={type}>
+            <span>주문하기</span> 
+        </BtnStyle>
+    )
+}
+
+const SoldoutBtn = ({type}) => {
+    return (
+        <BtnStyle type={type}>
+            <span>일시품절</span> 
+        </BtnStyle>
+    )
+}
+
+const Button = ({ type }) => {
+    return {
+        order: <OrderBtn type={ type } />,
+        soldout: <SoldoutBtn type={ type } />
+    }[type];
+}
+
+export default Button;
