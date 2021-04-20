c6effa36fb9177218a864403add224f48b2e9705
Song Lee <songlish233@gmail.com>
2021-04-20 23:17:35 +0900

    feat: SideDishes Entity 사용성 개선
    - Category enum 생성하여 카테고리 설정 안정화
    - SideDishFindable 인터페이스 설정하여 의존성 개선

--- ios/sidedish/sidedish/Domain/Entity/SideDishes.swift
+++ ios/sidedish/sidedish/Domain/Entity/SideDishes.swift
@@ -9,15 +9,25 @@ import Foundation
 
 class SideDishes: Codable {
     
-    private(set) var category: String
+    private(set) var category: Category
+    
+    enum Category: String, Codable {
+        case main = "한그릇 뚝딱 메인 요리"
+        case soup = "김이 모락모락 국.찌개"
+        case side = "언제 먹어도 든든한 밑반찬"
+    }
+    
     private var list: [SideDish]
     
-    init(category: String, list: [SideDish]) {
+    init(category: Category, list: [SideDish]) {
         self.category = category
         self.list = list
     }
     
-    //프로토콜로 분리
+}
+
+extension SideDishes: SideDishFindable {
+    
     func count() -> Int {
         return list.count
     }
