d41ef214169d012306e7990e12b037ff708e158c
JinseoPark <rarebook92@naver.com>
2021-04-20 12:26:59 +0900

    feat : add collectionView

--- BanchanCode/BanchanCode/MainViewController.swift
+++ BanchanCode/BanchanCode/MainViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var dishCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
