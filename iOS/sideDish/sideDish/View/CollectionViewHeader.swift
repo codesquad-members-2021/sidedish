9eb2eb6c4031bb5b6253e71c71010dd36e7c1624
Ray suh <readingod@naver.com>
2021-04-20 15:57:53 +0900

    #1 feat::hamster: SectionHeader AddGesture(TapGesture)

--- iOS/sideDish/sideDish/View/CollectionViewHeader.swift
+++ iOS/sideDish/sideDish/View/CollectionViewHeader.swift
@@ -7,11 +7,13 @@ class CollectionViewHeader: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureHeaderLabel()
+        setupTapGesture()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureHeaderLabel()
+        setupTapGesture()
     }
     
     private func configureHeaderLabel() {
@@ -23,5 +25,14 @@ class CollectionViewHeader: UICollectionReusableView {
         headerLabel.heightAnchor.constraint(equalToConstant: 32).isActive = true
         headerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
         headerLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
+    }
+    
+    private func setupTapGesture() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapSectionHeader(_:)))
+        addGestureRecognizer(gesture)
+    }
+    
+    @objc private func didTapSectionHeader(_ gesture: UITapGestureRecognizer) {
+       print("요구사항은 Toaster쓰는게 아닌거 같은데...흠")
     }
 }
