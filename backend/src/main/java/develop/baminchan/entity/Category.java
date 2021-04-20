1e367d5d1bc1d0bda7054bd6c50b074dc2d71c7c
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-04-20 21:58:38 +1200

    fix: Category 수정

--- backend/src/main/java/develop/baminchan/entity/Category.java
+++ backend/src/main/java/develop/baminchan/entity/Category.java
@@ -1,28 +1,38 @@
 package develop.baminchan.entity;
 
+import org.springframework.data.annotation.Id;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
-    private String categoryId;
-    private String name;
-    private List<Banchan> banchans = new ArrayList<>();
+    @Id
+    private int id;
 
-    public Category(String categoryId, String name, List<Banchan> banchans) {
-        this.categoryId = categoryId;
+    private int category_id;
+    private String name;
+    private List<Banchan> items = new ArrayList<>();
+
+    public Category(int id, int category_id, String name, List<Banchan> items) {
+        this.id = id;
+        this.category_id = category_id;
         this.name = name;
-        this.banchans = banchans;
+        this.items = items;
     }
 
-    public String getCategoryId() {
-        return categoryId;
+    public int getId() {
+        return id;
+    }
+
+    public int getCategory_id() {
+        return category_id;
     }
 
     public String getName() {
         return name;
     }
 
-    public List<Banchan> getBanchans() {
-        return banchans;
+    public List<Banchan> getItems() {
+        return items;
     }
 }
