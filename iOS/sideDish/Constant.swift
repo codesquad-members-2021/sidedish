9aa81842f5b5d51998c7ed21f394e5b92b416de6
Ray suh <readingod@naver.com>
2021-04-20 12:06:41 +0900

    #1 feat::hamster: CollectionView Delegate && DataSource

---
+++ iOS/sideDish/Constant.swift
@@ -0,0 +1,10 @@
+enum CellIdentifier {
+    static let foodCell = "foodCell"
+    static let foodHeader = "foodHeader"
+}
+
+enum HeaderInfo {
+    static let main = "모두가 좋아하는 든든한 메인요리"
+    static let soup = "정성이 담긴 뜨근뜨근 국물요리"
+    static let side = "식탁을 풍성하게 하는 정갈한 밑반찬"
+}
--- iOS/sideDish/sideDish/Controller/MainViewController.swift
+++ iOS/sideDish/sideDish/Controller/MainViewController.swift
@@ -17,7 +17,11 @@ extension MainViewController {
         mainCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout.init())
         view.addSubview(mainCollectionView)
         configureMainCollectionView()
+        mainCollectionView.dataSource = self
+        mainCollectionView.delegate = self
+        mainCollectionView.register(FoodCell.self, forCellWithReuseIdentifier: CellIdentifier.foodCell)
     }
+    
     private func configureMainCollectionView() {
         mainCollectionView.backgroundColor = UIColor.clear
         let layout = UICollectionViewFlowLayout()
@@ -29,5 +33,41 @@ extension MainViewController {
         mainCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
         mainCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         
+    }
+}
+
+//MARK: -CollectionView DataSource && Delegate
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    //Cell Count for Each Section
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5 // test code
+    }
+    
+    //Cell Information
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifier.foodCell, for: indexPath) as! FoodCell
+        //Start TestCode
+        cell.foodImageView.image = UIImage(named: "side")
+        cell.foodInfoStackView.foodNameLabel.text = "[마샐미디쉬] 매콤마늘쫑 해산물볶음 180G"
+        cell.foodInfoStackView.foodDescriptionLabel.text = "탱글탱글한 새우와 오징어를 .."
+        cell.foodInfoStackView.sellPriceLabel.text = "6,210원"
+        //End TestCode
+        return cell
+    }
+    
+    //Cell Size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 343, height: 130)
+    }
+    
+    //Cell Edge Insets
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 15, bottom: 30, right: 15)
+    }
+    
+    //Number of Sections
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
     }
 }
