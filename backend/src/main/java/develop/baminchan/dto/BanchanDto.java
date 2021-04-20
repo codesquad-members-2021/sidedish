c81ab8ff47afd1c0f2d20b7092c89d6143cf989e
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-04-21 09:59:38 +1200

    add : BanchanDto 생성
    1. 배열 반환을 위해
    Entity는 디비와 1:1매칭이 된다. 배열 반환을 위해서 타입을 배열로 주면 MySQL에서 데이터타입으로 배열로 설정할수 없기때문에 데이터를 집어넣을수없다.
    
    2. 디비는 Entity로, 나머지는 DTO로 데이터를 통신.

---
+++ backend/src/main/java/develop/baminchan/dto/BanchanDto.java
@@ -0,0 +1,77 @@
+package develop.baminchan.dto;
+
+import develop.baminchan.entity.Banchan;
+
+public class BanchanDto {
+    private String detail_hash;
+    private String image;
+    private String alt;
+    private String[] delivery_type;
+    private String title;
+    private String description;
+    private String n_price;
+    private String s_price;
+    private String[] badge;
+
+    protected BanchanDto() {
+
+    }
+
+    private BanchanDto(Banchan banchan) {
+        this.detail_hash = banchan.getDetail_hash();
+        this.image = banchan.getImage();
+        this.alt = banchan.getAlt();
+        this.delivery_type = convertStringToArray(banchan.getDelivery_type());
+        this.title = banchan.getTitle();
+        this.description = banchan.getDescription();
+        this.n_price = banchan.getN_price();
+        this.s_price = banchan.getS_price();
+        this.badge = convertStringToArray(banchan.getBadge());
+    }
+
+    private String[] convertStringToArray(String str) {
+        String[] arr = str.split(",");
+        return arr;
+    }
+
+    // Entity -> DTO
+    public static BanchanDto of(Banchan banchan) {
+        return new BanchanDto(banchan);
+    }
+
+    public String getDetail_hash() {
+        return detail_hash;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getAlt() {
+        return alt;
+    }
+
+    public String[] getDelivery_type() {
+        return delivery_type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getN_price() {
+        return n_price;
+    }
+
+    public String getS_price() {
+        return s_price;
+    }
+
+    public String[] getBadge() {
+        return badge;
+    }
+}
