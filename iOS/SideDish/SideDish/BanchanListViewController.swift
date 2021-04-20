ceb0ee367a80505f0bb021738b7e0d21380239da
shimyoungmin <dudals6965@gmail.com>
2021-04-20 14:19:34 +0900

    feat: NavigationController & ViewController added
    반찬 컬렉션뷰를 나타낼 BanchanListViewController 와 상세페이지를 나타내는 DetailBanchanViewController 추가

--- iOS/SideDish/SideDish/BanchanListViewController.swift
+++ iOS/SideDish/SideDish/BanchanListViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BanchanListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
---
+++ iOS/SideDish/SideDish/DetailBanchanViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailBanchanViewController.swift
+//  SideDish
+//
+//  Created by 심영민 on 2021/04/20.
+//
+
+import UIKit
+
+class DetailBanchanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
