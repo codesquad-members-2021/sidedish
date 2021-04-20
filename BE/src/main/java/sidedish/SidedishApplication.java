a87f94c474c63e35629bae41cbca6744feac70b9
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-04-20 11:19:39 +0900

    refactor: change directory

--- BE/src/main/java/sidedish/SidedishApplication.java
+++ BE/src/main/java/sidedish/SidedishApplication.java
@@ -1,13 +1,13 @@
-package be.demo;
+package sidedish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SidedishApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SidedishApplication.class, args);
     }
 
 }
---
+++ BE/src/main/java/sidedish/domain/Category.java
@@ -0,0 +1,4 @@
+package sidedish.domain;
+
+public class Category {
+}
