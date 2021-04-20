31d895961950d9e1a76ff1fab61d3c30c88e348b
JinseoPark <rarebook92@naver.com>
2021-04-20 15:27:47 +0900

    add custom collectionViewCell

--- iOS/BanchanCode/BanchanCode/DishCell.swift
+++ iOS/BanchanCode/BanchanCode/DishCell.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class DishCell: UICollectionViewCell {
 
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var discountPriceLabel: UILabel!
+    
+    @IBOutlet weak var originalPriceLabel: UILabel!
+    
+    @IBOutlet weak var badgeBackgroundView: UIView!
+    
+    @IBOutlet weak var badgeLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
