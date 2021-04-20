54a0914a904b1d8a1f6aefe6a720ff5fd94843b2
Jaeuk-YOO <ryou9305@naver.com>
2021-04-20 22:13:39 +0900

    fix: carousel 컴포넌트 좌우 이동 함수 skeleton

--- FE/src/Components/Carousel/CarouselButton.js
+++ FE/src/Components/Carousel/CarouselButton.js
@@ -1,10 +1,31 @@
-const CarouselButton = ({ type, dispatch }) => {
+import styled from 'styled-components';
+
+import LeftArrowButtonActive from '../commons/Buttons/LeftArrowButtonActive.js';
+import RightArrowButtonActive from '../commons/Buttons/RightArrowButtonActive.js';
+
+const CarouselButton = ({ type, onDispatch }) => {
+  
+  const selectButton = () => {
+    switch (type) {
+      case "right":
+        return <RightArrowButtonActive />;
+      case "left":
+        return <LeftArrowButtonActive />;
+    }
+  }
   
   return (
-    <div>
-      
-    </div>
+    <ButtonBox onClick={() => onDispatch({ action: type })}>
+      {selectButton()}
+    </ButtonBox>
   )
 }
+
+const ButtonBox = styled.div`
+  cursor: pointer;
+  
+  display: flex;
+  align-items: center;
+`
 
 export default CarouselButton
