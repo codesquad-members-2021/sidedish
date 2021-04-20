83e455b68bc5cf87d4563ddce53e16be744d5ac7
Sonjh1306 <yale1306@gmail.com>
2021-04-20 15:54:38 +0900

    feat: Create Cell
    Create CollectionView and add prototype cells to Custom style.
    Apply the CollectionViewCell to subviews based on ContentView.

---
+++ frontend/SideDish/SideDish/MenuCell.swift
@@ -0,0 +1,33 @@
+//
+//  MenuCell.swift
+//  SideDish
+//
+//  Created by sonjuhyeong on 2021/04/20.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    @IBOutlet weak var pastPriceLabel: UILabel!
+    @IBOutlet weak var eventLabel: UILabel!
+    @IBOutlet weak var launchingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configure()
+    }
+    
+    func configure() {
+        self.eventLabel.layer.masksToBounds = true
+        self.eventLabel.layer.cornerRadius = 5
+        self.launchingLabel.layer.masksToBounds = true
+        self.launchingLabel.layer.cornerRadius = 5
+        self.titleLabel.text = "[미노리키친] 일본식 우엉조림 (킨피라고보) 80g"
+    }
+
+}
--- frontend/SideDish/SideDish/ViewController.swift
+++ frontend/SideDish/SideDish/ViewController.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sideDishCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let nibName = UINib(nibName: "MenuCell", bundle: .none)
+        sideDishCollectionView.register(nibName, forCellWithReuseIdentifier: "menuCell")
+        sideDishCollectionView.dataSource = self
+        sideDishCollectionView.delegate = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "menuCell", for: indexPath) as?  MenuCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
