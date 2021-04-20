a85517d9f920553b316206d02238e9d2832506dd
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-04-20 16:17:59 +0900

    [Fix]:Best 데이터 fetch 오류 수정

--- sidedish/src/component/BestSidedish/BestSidedish.jsx
+++ sidedish/src/component/BestSidedish/BestSidedish.jsx
@@ -1,40 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BestTabList from 'component/BestSidedish/BestTabList/BestTabList';
 import BestSidedishList from 'component/BestSidedish/BestSidedishList/BestSidedishList';
 import useFetch from 'hooks/useFetch';
 
 const BestSidedish = () => {
-  const { data: bestList, loading } = useFetch(
-    `https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/best`
-  );
-  console.log('loading: ', loading);
-  const parsedBestList = bestList
-    ? bestList.body.map(({ category_id, name }) => {
-        return {
-          id: category_id,
-          title: name,
-        };
-      })
-    : [];
-  console.log('parsingValue: ', parsedBestList);
-  const [focusedCategory, setFocusedCategory] = useState(bestList && parsedBestList[0].id);
+  const [bestList, setBestList] = useState(null);
+  const [focusedCategory, setFocusedCategory] = useState(null);
 
-  return loading ? (
+  useEffect(() => {
+    initBestSidedish(
+      `https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/best`
+    );
+  }, []);
+
+  const initBestSidedish = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const parsedData = parseBestList(data);
+    setBestList(parsedData);
+    setFocusedCategory(parsedData[0].id);
+  };
+
+  return (
     <div>
-      <BestTabList bestList={parsedBestList} />
-      <BestSidedishList focusedCategory={focusedCategory} />
+      {bestList && <BestTabList bestList={bestList} />}
+      {focusedCategory && <BestSidedishList focusedCategory={focusedCategory} />}
     </div>
-  ) : (
-    <span>Loading...</span>
   );
 };
 
-export default BestSidedish;
+const parseBestList = (data) => {
+  if (!data || !data.body) return;
+  return data.body.map(({ category_id, name }) => {
+    return {
+      id: category_id,
+      title: name,
+    };
+  });
+};
 
-// const data = [
-//   { id: 17011200, title: '할인특가 세트상품' },
-//   { id: 17011000, title: '풍성한 고기반찬' },
-//   { id: 17010200, title: '편리한 반찬세트' },
-//   { id: 17010300, title: '간편한 덮밥요리' },
-//   { id: 17011400, title: '우리아이 영양반찬' },
-// ];
+export default BestSidedish;
--- sidedish/src/hooks/useFetch.js
+++ sidedish/src/hooks/useFetch.js
@@ -4,20 +4,20 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const fetchData = async (url, cb) => {
+  const fetchData = async (url) => {
     try {
       setLoading(true);
       const res = await fetch(url);
       const data = await res.json();
       setLoading(false);
-      cb(data);
+      setData(data);
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    fetchData(url, setData);
+    fetchData(url);
   }, []);
 
   return { data, loading };
