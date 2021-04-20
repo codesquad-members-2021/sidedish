8dd0e32121459266bf50c20c57be84e84f72156e
GleamingStar <tjdduddl10@gmail.com>
2021-04-20 21:44:55 +0900

    feat: Add Badge.jsx

---
+++ src/js/components/common/Badge.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const BadgeWrapper = styled.ul`
+	display: flex;
+	flex-direction: row;
+	align-items: flex-start;
+	padding: 0px;
+
+	position: static;
+`;
+
+const Tag = styled.li`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+
+	position: static;
+	width: ${(props) => (props.type === "이벤트특가" ? "97px" : "84px")};
+	height: 28px;
+	background: ${(props) => (props.type === "이벤트특가" ? "#82d32d" : "#86C6FF")};
+	border-radius: 5px;
+
+	font-family: Noto Sans KR;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 14px;
+	line-height: 20px;
+
+	color: #ffffff;
+`;
+
+const Badge = ({ badge }) => <BadgeWrapper>{badge.map(e => <Tag key={e} type={e}>{e}</Tag>)}</BadgeWrapper>;
+
+export default Badge;
