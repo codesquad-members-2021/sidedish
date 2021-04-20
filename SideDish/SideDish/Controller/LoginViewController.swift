6a483f9a8213c39ab88bd5638359e0f13808288d
zeke <yangsin1031@naver.com>
2021-04-20 12:13:18 +0900

    feat: add loginviewcontroller and octokit

---
+++ SideDish/SideDish/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  SideDish
+//
+//  Created by 양준혁 on 2021/04/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func access(_ sender: UIButton) {
+    }
+    
+}
