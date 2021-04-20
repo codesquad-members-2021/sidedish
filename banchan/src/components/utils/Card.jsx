587b29aa4047666d29bf8a2215a4079a513199f3
Hongbi <dyongdi@gmail.com>
2021-04-20 20:30:35 +0900

    [Refactor] Styled component 이름 앞에 Styled 키워드를 붙임

--- banchan/src/components/utils/Card.jsx
+++ banchan/src/components/utils/Card.jsx
@@ -3,71 +3,71 @@ import theme from './constant';
 
 const Card = ({ product, cardSize }) => {
   return (
-    <CardContainer cardSize={cardSize}>
-      <img src={product.image} alt="card-image" />
-      <CardInfoContainer>
-        <div className="title">
+    <StyledLi cardSize={cardSize}>
+      <StyledImg cardSize={cardSize} src={product.image} alt="card-image" />
+      <StyledCardInfoContainer>
+        <StyledTitle>
           <span>{product.title}</span>
-        </div>
-        <div className="description">
+        </StyledTitle>
+        <StyledDescription>
           <span>{product.description}</span>
-        </div>
+        </StyledDescription>
         <Price product={product} />
-      </CardInfoContainer>
-    </CardContainer>
+      </StyledCardInfoContainer>
+    </StyledLi>
   );
 };
 
 const Price = ({ product }) => {
   const isOnSale = !!product.n_price;
   if (!isOnSale) {
-    return <FinalPriceSpan>{product.s_price}</FinalPriceSpan>;
+    return <StyledFinalPriceSpan>{product.s_price}</StyledFinalPriceSpan>;
   }
   return (
-    <div className="price">
-      <FinalPriceSpan>{product.n_price}</FinalPriceSpan>
-      <OriginalPriceSpan>{product.s_price}</OriginalPriceSpan>
-    </div>
+    <StyledPriceWrapper className="price">
+      <StyledFinalPriceSpan>{product.n_price}</StyledFinalPriceSpan>
+      <StyledOriginalPriceSpan>{product.s_price}</StyledOriginalPriceSpan>
+    </StyledPriceWrapper>
   );
 };
 
 // ============================= ● Styled components ● =============================
 
-const CardContainer = styled.li`
+const StyledLi = styled.li`
   width: ${(props) => (props.cardSize === 'medium' ? '308px' : '384px')};
-
-  img {
-    border-radius: ${theme.borders.radius};
-    margin-bottom: 16px;
-    width: ${(props) => (props.cardSize === 'medium' ? '308px' : '384px')};
-  }
 `;
 
-const CardInfoContainer = styled.div`
-  .title {
-    font-size: ${theme.fontSizes.S};
-    color: ${theme.colors.darkGray};
-  }
-
-  .description {
-    font-size: ${theme.fontSizes.XS};
-    color: ${theme.colors.gray};
-    margin: 8px 0px;
-  }
-
-  .price {
-    display: flex;
-    align-items: center;
-  }
+const StyledImg = styled.img`
+  border-radius: ${theme.borders.radius};
+  margin-bottom: 16px;
+  width: ${(props) => (props.cardSize === 'medium' ? '308px' : '384px')};
 `;
 
-const FinalPriceSpan = styled.span`
+const StyledCardInfoContainer = styled.div``;
+
+const StyledTitle = styled.div`
+  font-size: ${theme.fontSizes.S};
+  color: ${theme.colors.darkGray};
+`;
+
+const StyledDescription = styled.div`
+  font-size: ${theme.fontSizes.XS};
+  color: ${theme.colors.gray};
+  margin: 8px 0px;
+`;
+
+const StyledPriceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledFinalPriceSpan = styled.span`
   font-size: ${theme.fontSizes.L};
   font-weight: bold;
   margin-right: 8px;
 `;
 
-const OriginalPriceSpan = styled.span`
+const StyledOriginalPriceSpan = styled.span`
   font-size: ${theme.fontSizes.XS};
   text-decoration: line-through;
   color: ${theme.colors.lightGray};
