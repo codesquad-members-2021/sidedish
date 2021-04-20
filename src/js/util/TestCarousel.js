c412b8bb01beb6562141c50ccd29bdf361802ece
jjunyjjuny <jjuny546@gmail.com>
2021-04-21 00:44:52 +0900

    temp : 멍청하게도 feature에 안 함

--- src/js/util/TestCarousel.js
+++ src/js/util/TestCarousel.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import Carousel from "./Carousel";
 
 const StyledBlock = styled.div`
-  background: skyblue;
+  background-color: #e3e3e3;
+  width: 700px;
 `;
 const TestBlock = styled.div`
   background-color: tan;
@@ -11,7 +12,12 @@ const TestCarousel = () => {
   return (
     <StyledBlock>
       <h2>테스트 중입니다</h2>
-      <Carousel itemPerPeice={3}>
+      <Carousel
+        itemsPerPeice={4}
+        onClickItem={() => {
+          console.log("click item!");
+        }}
+      >
         <TestBlock>test1</TestBlock>
         <TestBlock>test2</TestBlock>
         <TestBlock>test3</TestBlock>
@@ -19,6 +25,19 @@ const TestCarousel = () => {
         <TestBlock>test5</TestBlock>
         <TestBlock>test6</TestBlock>
         <TestBlock>test7</TestBlock>
+        <TestBlock>test8</TestBlock>
+        <TestBlock>test9</TestBlock>
+        <TestBlock>test10</TestBlock>
+        <TestBlock>test11</TestBlock>
+        <TestBlock>test12</TestBlock>
+        <TestBlock>test13</TestBlock>
+        <TestBlock>test14</TestBlock>
+        <TestBlock>test15</TestBlock>
+        <TestBlock>test16</TestBlock>
+        <TestBlock>test17</TestBlock>
+        <TestBlock>test18</TestBlock>
+        <TestBlock>test19</TestBlock>
+        <TestBlock>test20</TestBlock>
       </Carousel>
     </StyledBlock>
   );
