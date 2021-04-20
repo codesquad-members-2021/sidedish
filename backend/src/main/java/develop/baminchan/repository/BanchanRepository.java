e3247f935ab201d876150815044e912c1c3c71cf
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-04-20 21:57:47 +1200

    add : detail_hash에 값으로 반찬 찾기

--- backend/src/main/java/develop/baminchan/repository/BanchanRepository.java
+++ backend/src/main/java/develop/baminchan/repository/BanchanRepository.java
@@ -4,6 +4,7 @@ import develop.baminchan.entity.Banchan;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BanchanRepository extends CrudRepository<Banchan, String> {
-
+public interface BanchanRepository extends CrudRepository<Banchan, Long> {
+    @Query("select * from banchan where detail_hash = :detail_hash")
+    Banchan findBanchanByDetail_hash(String detail_hash);
 }
