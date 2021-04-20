692c4879ee0c72232e4dc0a4b225863b0653e857
dudn1933 <dudn1933@naver.com>
2021-04-20 11:33:46 +0900

    chore folder 구성

---
+++ FE/src/Components/Best/Best.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Best = () => {
+  return <div></div>;
+};
+
+export default Best;
---
+++ FE/src/Components/Main/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <div>zzz 스타트</div>;
+};
+
+export default Main;
