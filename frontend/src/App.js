a0529758108910265b651fa0cba00269ba88cbb2
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-20 23:01:49 +0900

    Chore. Category 컴포넌트 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
-
+import Header from './components/header/Header';
+import Category from './components/category/Category';
+import MainMenu from  './components/mainmenu/MainMenu'
 function App() {
   return (
-  
+  <>
+  {/* <Header/>
+  <MainMenu/> */}
+  <Category/>
+  </>
   );
 }
 
---
+++ frontend/src/components/category/Category.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import CategorySlide from './CategorySlide'
+
+const CategoryDiv = styled.div`
+background-color: black;
+` 
+
+const CategoryBtn = styled.button``
+
+
+function Category() {
+  return (
+    <CategoryDiv>
+    <CategoryBtn/>
+    </CategoryDiv>
+  );
+}
+
+export default Category;
