475646f6aafb63816b5362e58be85f82160ff36a
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-04-20 18:53:01 +0900

    feat: add Category entity and enum CategoryType

--- BE/src/main/java/sidedish/domain/Category.java
+++ BE/src/main/java/sidedish/domain/Category.java
@@ -1,4 +1,29 @@
 package sidedish.domain;
 
+import org.springframework.data.annotation.Id;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Category {
+
+    @Id
+    private Long id;
+
+    private CategoryType categoryType;
+    private Set<Dish> dishes;
+
+    public Category(CategoryType categoryType) {
+        this.categoryType = categoryType;
+        this.dishes = new HashSet<>();
+    }
+
+    public CategoryType getTitle() {
+        return categoryType;
+    }
+
+    public Set<Dish> getDishes() {
+        return dishes;
+    }
+
 }
---
+++ BE/src/main/java/sidedish/domain/CategoryType.java
@@ -0,0 +1,13 @@
+package sidedish.domain;
+
+public enum CategoryType {
+    MAIN("모두가 좋아하는 든든한 메인요리"),
+    SOUP("정성이 담긴 뜨끈뜨끈 국물요리"),
+    SIDE("식탁을 풍성하게 하는 정갈한 밑반찬");
+
+    private String title;
+
+    CategoryType(String title) {
+        this.title = title;
+    }
+}
