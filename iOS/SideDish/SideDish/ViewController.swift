f2723b610d58bab7a00d67d4628b2b72072fae36
shimyoungmin <dudals6965@gmail.com>
2021-04-20 14:07:10 +0900

    feat: Create Project

---
+++ iOS/SideDish/SideDish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideDish
+//
+//  Created by 심영민 on 2021/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
