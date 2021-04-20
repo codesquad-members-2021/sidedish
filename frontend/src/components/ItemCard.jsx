b6332cb131248b96b07215c5e85f083c90377372
junamee <skawn11114@gmail.com>
2021-04-20 21:07:07 +0900

    Feat. MainMenu 마크업, itemCard 샘플 마크업#9

---
+++ frontend/src/components/ItemCard.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import mokData from "./mokdata";
+
+const Card = styled.div``;
+const IMG = styled.img`
+  width: ${(props) => {
+    return props.size === "L" ? "384px" : "308px";
+  }};
+  height: ${(props) => {
+    return props.size === "L" ? "384px" : "308px";
+  }};
+  margin-bottom: 16px;
+`;
+const ItemTitle = styled.p``;
+const ItemDesc = styled.p``;
+const ItemPrice = styled.p``;
+const ItemPriceNormal = styled.p``;
+const Badge = styled.p``;
+
+function ItemCard() {
+  return (
+    <>
+      {mokData.map((data) => (
+        <Card size="L">
+          <IMG size="L" src={data.image} alt={data.alt}></IMG>
+          <ItemTitle>{data.alt}</ItemTitle>
+          <ItemDesc>{data.description}</ItemDesc>
+          <ItemPrice>{data.s_price ? data.s_price : data.p_price}</ItemPrice>
+
+          <ItemPriceNormal>{data.s_price && data.n_price}원</ItemPriceNormal>
+          <Badge></Badge>
+        </Card>
+      ))}
+    </>
+  );
+}
+
+export default ItemCard;
