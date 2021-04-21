19938d8873fff35603f51f969f3c3c2e7db1118c
gisungchoi <chlrltjd159@naver.com>
2021-04-21 15:07:03 +0900

    test : event item test
    - item 조회 시 event item도 조회 되어야 함

--- be/sidedish/src/test/java/com/codesquad/sidedish/category/domain/SidedishCategoryRepositoryTest.java
+++ be/sidedish/src/test/java/com/codesquad/sidedish/category/domain/SidedishCategoryRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.codesquad.sidedish.category.domain;
 
+import com.codesquad.sidedish.event.SidedishEvent;
+import com.codesquad.sidedish.event.SidedishEventRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -17,6 +19,9 @@ class SidedishCategoryRepositoryTest {
 
     @Autowired
     CategoryRepository categoryRepository;
+
+    @Autowired
+    SidedishEventRepository eventRepository;
 
     Logger logger = LoggerFactory.getLogger(SidedishCategoryRepositoryTest.class);
 
@@ -38,8 +43,13 @@ class SidedishCategoryRepositoryTest {
     void createItem() {
         SidedishCategory sidedishCategory = new SidedishCategory("메인요리");
 
+        SidedishEvent event = createSidedishEvent("점심 특가", "#ffffff");
+
         SidedishItem sidedishItem1 = createSidedishItem("맛있는 반찬");
         SidedishItem sidedishItem2 = createSidedishItem("상큼한 반찬");
+
+        sidedishItem1.addEvent(event);
+        sidedishItem2.addEvent(event);
 
         sidedishCategory.addSidedishItem(sidedishItem1);
         sidedishCategory.addSidedishItem(sidedishItem2);
@@ -61,5 +71,10 @@ class SidedishCategoryRepositoryTest {
                 4000, 5000, 3, 1,
                 "서울 경기 새벽배송 / 전국택배 (제주 및 도서산간 불가)",
                 "2,500원 (40,000원 이상 구매 시 무료)");
+    }
+
+
+    private SidedishEvent createSidedishEvent (String eventName,String eventColor) {
+        return eventRepository.save(new SidedishEvent(eventName,eventColor));
     }
 }
