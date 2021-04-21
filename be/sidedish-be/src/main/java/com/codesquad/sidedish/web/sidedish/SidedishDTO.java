56d2a8d3642d3247027d0e85b0eff18dc6305f67
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-04-21 16:24:18 +0900

    [FEAT] : SidedishDTO 생성 및 json 읽어서 객체 생성할 수 있는 기능 추가
    jackson object mapper 사용. json은 String 혹은 파일로 입력 가능. json파일은 개발 환경에만 들어갈 것이므로 Environment를 이용하여 환경에 따라 테스트 로직 실행

---
+++ be/sidedish-be/src/main/java/com/codesquad/sidedish/web/sidedish/SidedishDTO.java
@@ -0,0 +1,106 @@
+package com.codesquad.sidedish.web.sidedish;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class SidedishDTO {
+    private long categoryId;
+    private String name;
+    private List<ItemDTO> items;
+
+    public SidedishDTO() {
+    }
+
+    public SidedishDTO(long categoryId, String name, List<ItemDTO> items) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.items = items;
+    }
+
+    public static List<SidedishDTO> listFrom(String jsonString) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        List<SidedishDTO> sidedishDTOS = objectMapper.readValue(jsonString, new TypeReference<List<SidedishDTO>>() {
+        });
+
+        return objectMapper.readValue(
+                jsonString,
+                new TypeReference<List<SidedishDTO>>() {
+                }
+        );
+    }
+
+    public static List<SidedishDTO> listFrom(File file) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        return objectMapper.readValue(
+                file,
+                new TypeReference<List<SidedishDTO>>() {
+                }
+        );
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ItemDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemDTO> items) {
+        this.items = items;
+    }
+
+    public static final class SidedishDTOBuilder {
+        private long categoryId;
+        private String name;
+        private List<ItemDTO> items;
+
+        private SidedishDTOBuilder() {
+        }
+
+        public static SidedishDTOBuilder create() {
+            return new SidedishDTOBuilder();
+        }
+
+        public SidedishDTOBuilder categoryId(long categoryId) {
+            this.categoryId = categoryId;
+            return this;
+        }
+
+        public SidedishDTOBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public SidedishDTOBuilder items(List<ItemDTO> items) {
+            this.items = items;
+            return this;
+        }
+
+        public SidedishDTO build() {
+            return new SidedishDTO(categoryId, name, items);
+        }
+    }
+}
