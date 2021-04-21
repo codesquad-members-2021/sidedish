c8bb102fd2a7cf0374d374ad699a1d2b93c1957e
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-04-21 16:46:46 +0900

    [FEAT] : BestSidedishController 추가
    베스트반찬 출력해주는 목업 api 추가. 현재는 SampleDataFactory에서 가져오도록 작성.

---
+++ be/sidedish-be/src/main/java/com/codesquad/sidedish/web/sidedish/BestSidedishController.java
@@ -0,0 +1,15 @@
+package com.codesquad.sidedish.web.sidedish;
+
+import com.codesquad.sidedish.utils.SampleDataFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BestSidedishController {
+    @GetMapping("/best")
+    public List<SidedishDTO> readBestSidedishes() {
+        return SampleDataFactory.createBestMenus();
+    }
+}
