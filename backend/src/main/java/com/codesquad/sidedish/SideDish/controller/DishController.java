885cd3c6b6cc3910961ff851530cee2592671174
고정완 (Can) <jw.gho@ecubelabs.com>
2021-04-22 08:54:18 +0900

    [docs] Swagger 로 API 문서화

--- backend/src/main/java/com/codesquad/sidedish/SideDish/controller/DishController.java
+++ backend/src/main/java/com/codesquad/sidedish/SideDish/controller/DishController.java
@@ -3,11 +3,18 @@ package com.codesquad.sidedish.SideDish.controller;
 import com.codesquad.sidedish.SideDish.dto.*;
 import com.codesquad.sidedish.SideDish.service.CategoryService;
 import com.codesquad.sidedish.SideDish.service.DishService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = {"Side Dish API"}, description = "Bat 와 Pyro 가 야근하며 만든 API")
 @RestController
 public class DishController {
     private final CategoryService categoryService;
@@ -18,44 +25,50 @@ public class DishController {
         this.dishService = dishService;
     }
 
-    @GetMapping
-    @RequestMapping("/categories")
+    @GetMapping("/categories")
+    @ApiOperation(value = "카테고리", notes = "요리 카테고리 목록을 반환합니다.")
     public ResponseEntity<List<CategoryDto>> getCategories() {
         List<CategoryDto> categories = categoryService.getList();
         return ResponseEntity.ok().body(categories);
     }
 
     @GetMapping("/detail/{detailHash}")
-    public ResponseEntity<DishDetailDto> getDetail(@PathVariable("detailHash") String detailHash) {
+    @ApiOperation(value = "요리 상세", notes = "요리의 상세 정보를 반환합니다.")
+    public ResponseEntity<DishDetailDto> getDetail(@ApiParam("요리의 식별자") @PathVariable("detailHash") String detailHash) {
         DishDetailDto dishDetailDto = dishService.getDetail(detailHash);
         return ResponseEntity.ok().body(dishDetailDto);
     }
 
     @GetMapping("/detail/{detailHash}/refreshable")
-    public ResponseEntity<RefreshDto> getDetailRefreshable(@PathVariable("detailHash") String detailHash, @RequestParam("lastUpdated") int lastUpdated) {
+    @ApiOperation(value = "요리 상세 갱신", notes = "요리의 상세 정보를 갱신할 필요가 있는지 여부를 반환합니다.")
+    public ResponseEntity<RefreshDto> getDetailRefreshable(@ApiParam("요리의 식별자") @PathVariable("detailHash") String detailHash, @ApiParam("프론트에서 마지막으로 업데이트 한 날짜. 데이터 형식: yyMMddhhmm") @RequestParam("lastUpdated") int lastUpdated) {
         RefreshDto refreshDto = dishService.getDetailRefreshable(detailHash, lastUpdated);
         return ResponseEntity.ok().body(refreshDto);
     }
 
     @GetMapping("/detail/{detailHash}/quantity")
-    public ResponseEntity<QuantityDto> getDetailQuantity(@PathVariable("detailHash") String detailHash) {
+    @ApiOperation(value = "요리 수량", notes = "요리의 주문 가능한 수량을 반환합니다.")
+    public ResponseEntity<QuantityDto> getDetailQuantity(@ApiParam("요리의 식별자") @PathVariable("detailHash") String detailHash) {
         QuantityDto quantityDto = dishService.getDetailQuantity(detailHash);
         return ResponseEntity.ok().body(quantityDto);
     }
 
     @GetMapping("/main")
+    @ApiOperation(value = "메인 요리", notes = "메인 요리의 목록을 반환합니다.")
     public ResponseEntity<List<DishDto>> getMainList() {
         List<DishDto> dishes = dishService.getList(1);
         return ResponseEntity.ok().body(dishes);
     }
 
     @GetMapping("/side")
+    @ApiOperation(value = "반찬 요리", notes = "반찬 요리의 목록을 반환합니다.")
     public ResponseEntity<List<DishDto>> getSideList() {
         List<DishDto> dishes = dishService.getList(1);
         return ResponseEntity.ok().body(dishes);
     }
 
     @GetMapping("/soup")
+    @ApiOperation(value = "국물 요리", notes = "국물 요리의 목록을 반환합니다.")
     public ResponseEntity<List<DishDto>> getSoupList() {
         List<DishDto> dishes = dishService.getList(1);
         return ResponseEntity.ok().body(dishes);
--- backend/src/main/java/com/codesquad/sidedish/SideDish/domain/CategoryMockRepository.java
+++ backend/src/main/java/com/codesquad/sidedish/SideDish/domain/CategoryMockRepository.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class CategoryMockRepository implements CategoryRepository {
     private final List<Category> categories = Arrays.asList(
-            new Category(1, "메인", "/main"),
-            new Category(2, "국", "/soup"),
-            new Category(3, "반찬", "/side")
+            new Category(1, "모두가 좋아하는 든든한 메인요리", "/main"),
+            new Category(2, "뜨끈하고 시원한 국물요리", "/soup"),
+            new Category(3, "밥도둑이 여기있다! 반찬요리", "/side")
     );
 
     @Override
