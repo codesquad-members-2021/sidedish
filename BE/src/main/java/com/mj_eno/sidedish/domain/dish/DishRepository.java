c887071f5fe03fabdabfcb30ff62c062368e0c87
jeong-inho <derosatam76@gmail.com>
2021-04-22 00:11:46 +0900

    Feat: DishRepository 인터페이스에 findAllByMenuCategoryId 메소드 생성

--- BE/src/main/java/com/mj_eno/sidedish/domain/dish/DishRepository.java
+++ BE/src/main/java/com/mj_eno/sidedish/domain/dish/DishRepository.java
@@ -2,5 +2,9 @@ package com.mj_eno.sidedish.domain.dish;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface DishRepository extends CrudRepository {
+import java.util.List;
+
+public interface DishRepository extends CrudRepository<Dish, Long> {
+
+    List<Dish> findAllByMenuCategoryId(Long menuCategoryId);
 }
