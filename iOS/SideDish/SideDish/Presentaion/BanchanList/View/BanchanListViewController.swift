a5f36c4f38050b3473d4ff29cfd12ba03e853128
Jisoo Kim <ghis22130@naver.com>
2021-04-21 12:15:50 +0900

    feat: Add BanchanListViewModel, UICollectionViewDiffableDataSource
    섹션별로 업데이트를 하기위해 UICollectionViewDiffableDataSource를 도입하였습니다.
    이에 따른 NSDiffableDataSourceSnapshot을 적용해 주었고 가독성을 높이기 위해 typealias를 통하여 재정의 해주었습니다.
    DataSource에 정보를 주기 위한 BanchanListViewModel을 정의해주었습니다.
    
    issue: #13

--- iOS/SideDish/SideDish/Presentaion/BanchanList/View/BanchanListViewController.swift
+++ iOS/SideDish/SideDish/Presentaion/BanchanList/View/BanchanListViewController.swift
@@ -8,10 +8,76 @@
 import UIKit
 
 class BanchanListViewController: UIViewController {
-
+    
+    @IBOutlet weak var banchanCollectionView: BanchanCollectionView!
+    
+    var viewModel = BanchanListViewModel()
+    lazy var dataSource = configureDataSource()
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Banchan>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Banchan>
+    typealias Section = BanchanListViewModel.Section
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        applySnapshot(animatingDifferences: false)
+        banchanCollectionView.dataSource = self.dataSource
+        banchanCollectionView.delegate = self
     }
 }
 
+// MARK: - DataSource
+extension BanchanListViewController {
+    func configureDataSource() -> DataSource {
+        let dataSource = DataSource(collectionView: banchanCollectionView) { (collectionView, indexPath, banchan) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BanchanCustomCell.identifer, for: indexPath) as? BanchanCustomCell else { return nil }
+            return cell
+        }
+        print("2")
+        dataSource.supplementaryViewProvider = { (collectionView, kind, indexPath) in
+            guard kind == UICollectionView.elementKindSectionHeader else { return nil }
+            let section = self.dataSource.snapshot().sectionIdentifiers[indexPath.section]
+            guard let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: BanchanCustomCellHeader.identifier, for: indexPath) as? BanchanCustomCellHeader else { return nil }
+            view.titleLabel.text = section.rawValue
+            return view
+            
+        }
+        return dataSource
+    }
+    
+    func applySnapshot(animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+       
+        snapshot.appendSections(Section.allCases)
+        
+        Section.allCases.forEach { section in
+            guard let banchans = viewModel.getBanchans(section: section) else { return }
+            snapshot.appendItems(banchans, toSection: section)
+        }
+        
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+}
+
+// MARK: - UICollectionViewDataSource Implementation
+//extension VideosViewController {
+//  override func collectionView(
+//    _ collectionView: UICollectionView,
+//    didSelectItemAt indexPath: IndexPath
+//  ) {
+//    guard let video = dataSource.itemIdentifier(for: indexPath) else {
+//      return
+//    }
+//    guard let link = video.link else {
+//      print("Invalid link")
+//      return
+//    }
+//    let safariViewController = SFSafariViewController(url: link)
+//    present(safariViewController, animated: true, completion: nil)
+//  }
+//}
+
+// MARK: - Delegate
+extension BanchanListViewController: UICollectionViewDelegate {
+    
+}
