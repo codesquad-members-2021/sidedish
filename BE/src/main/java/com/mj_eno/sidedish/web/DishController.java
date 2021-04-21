cfacd327eae38f21dc412e57076b713ded6b9cde
jeong-inho <derosatam76@gmail.com>
2021-04-22 00:11:00 +0900

    Feat: DishController 클래스에 모든 메인 반찬을 보여주기 위해 viewBestDishes 메소드 생성

--- BE/src/main/java/com/mj_eno/sidedish/web/DishController.java
+++ BE/src/main/java/com/mj_eno/sidedish/web/DishController.java
@@ -1,9 +1,13 @@
 package com.mj_eno.sidedish.web;
 
 import com.mj_eno.sidedish.service.DishService;
+import com.mj_eno.sidedish.web.dto.MainDishResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DishController {
@@ -13,5 +17,11 @@ public class DishController {
 
     public DishController(DishService dishService) {
         this.dishService = dishService;
+    }
+
+    @GetMapping("/main")
+    public List<MainDishResponseDTO> viewBestDishes() {
+        logger.info("모든 메인 반찬 요청");
+        return dishService.findAllMainDish();
     }
 }
