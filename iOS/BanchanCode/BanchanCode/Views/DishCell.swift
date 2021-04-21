921f2cce202342f30389d734b0820e6ae08250a3
JinseoPark <rarebook92@naver.com>
2021-04-21 18:18:10 +0900

    feat : add cornerRadius to image and badge (#4,#6)

--- iOS/BanchanCode/BanchanCode/Views/DishCell.swift
+++ iOS/BanchanCode/BanchanCode/Views/DishCell.swift
@@ -10,17 +10,11 @@ import UIKit
 class DishCell: UICollectionViewCell {
 
     @IBOutlet weak var thumbnailImageView: UIImageView!
-    
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var descriptionLabel: UILabel!
-    
     @IBOutlet weak var discountPriceLabel: UILabel!
-    
     @IBOutlet weak var originalPriceLabel: UILabel!
-    
     @IBOutlet weak var badgeBackgroundView: UIView!
-    
     @IBOutlet weak var badgeLabel: UILabel!
     
     static let identifier = "DishCell"
@@ -30,6 +24,13 @@ class DishCell: UICollectionViewCell {
         
         thumbnailImageView.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
         nameLabel.text = "Jackson!!!!"
+        
+        thumbnailImageView.layer.masksToBounds = true
+        thumbnailImageView.layer.cornerRadius = 5.0
+        
+        badgeBackgroundView.layer.masksToBounds = true
+        badgeBackgroundView.layer.cornerRadius = 5.0
+        
     }
 
 }
