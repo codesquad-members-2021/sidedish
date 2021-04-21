1d3df8a8683f8a30785395c53a4c6a125169d170
dong <ultraraptor@naver.com>
2021-04-21 17:33:12 +0900

    item 컨트롤러 생성해 기본적인 API 생성

--- BE/side-dish-backend/src/main/java/com/codesquad/team14/controller/ItemController.java
+++ BE/side-dish-backend/src/main/java/com/codesquad/team14/controller/ItemController.java
@@ -1,4 +1,16 @@
 package com.codesquad.team14.controller;
 
+import com.codesquad.team14.domain.Item;
+import com.codesquad.team14.dto.ItemDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ItemController {
+
+    @GetMapping("/best")
+    public ItemDto bestLists() {
+        return (new ItemDto("tempp"));
+    }
+
 }
