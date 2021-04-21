5849e546e42db0c35133e4fba40c7ba27a6d35d7
malaheaven <yunhg95@gmail.com>
2021-04-21 18:16:01 +0900

    Feat: repository를 생성해라
    - ItemRepository를 생성해라
    - ImageRepository를 생성해라
    - DishCategoryRepository를 생성해라
    - BestCategoryRepository를 생성해라

---
+++ backend/sidedish/src/main/java/codsquad/team17/sidedish/repository/BestCategoryRepository.java
@@ -0,0 +1,8 @@
+package codsquad.team17.sidedish.repository;
+
+
+import codsquad.team17.sidedish.domain.BestCategory;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BestCategoryRepository extends CrudRepository<BestCategory, Long> {
+}
