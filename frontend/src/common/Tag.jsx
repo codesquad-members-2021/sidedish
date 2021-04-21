7316493c2358b042c45f9ed11e047dbb9d8a27fc
eve712 <62237639+eve712@users.noreply.github.com>
2021-04-21 15:51:36 +0900

    style: 태그 간 간격 스타일 추가

--- frontend/src/common/Tag.jsx
+++ frontend/src/common/Tag.jsx
@@ -5,6 +5,7 @@ const StyledTag = styled.div`
     box-sizing: border-box;
     padding: 7px 16px 5px;
     border-radius: 5px;
+    margin-right: 10px;
     background: ${({type}) => (type === '이벤트특가') ? '#82D32D' : '#86C6FF'};
     color: #fff;
     font-size: 14px;
