594a4dc280a639aa9530db353e87abd10f69bcd7
Tree <gmldbs1109@naver.com>
2021-04-21 17:18:14 +0900

    [#18]Feat : Create DishController
    - DishController 생성 및 테스트 코드 추가

---
+++ backend/src/main/java/com/team15/sidedish/controller/DishController.java
@@ -0,0 +1,39 @@
+package com.team15.sidedish.controller;
+
+import com.team15.sidedish.domain.Dish;
+import com.team15.sidedish.service.DishService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DishController {
+    private final Logger logger = LoggerFactory.getLogger(DishController.class);
+    private final DishService dishService;
+
+    public DishController(DishService dishService) {
+        this.dishService = dishService;
+    }
+
+    @GetMapping("/dishes")
+    public List<Dish> showDishByHash() {
+        logger.info("Test show dishes");
+        return dishService.showDishes();
+    }
+
+    @GetMapping("/dish/{hash}")
+    public Dish showSingleDish(@PathVariable String hash) {
+        logger.info("Show single dish by hash");
+        return dishService.showSingleDish(hash);
+
+    }
+
+//    @GetMapping("/dish/{sectionName}")
+//    public List<Dish> showDishesBySectionName(@PathVariable String sectionName) {
+//        return dishService.showDishesBySectionName(sectionName);
+//    }
+}
