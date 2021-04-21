8680c9a25cf9bdacd00f03dfda14b76d704938e5
hoonha choi <sosqsz2@naver.com>
2021-04-21 17:54:59 +0900

    Feat: FoodCardHeadView 생성

---
+++ Side-dish/Side-dish/PresentationLayer/View/FoodCardHeaderView.swift
@@ -0,0 +1,39 @@
+//
+//  FoodCardHeaderView.swift
+//  Side-dish
+//
+//  Created by HOONHA CHOI on 2021/04/21.
+//
+
+import UIKit
+
+class FoodCardHeaderView: UICollectionReusableView {
+
+    @IBOutlet weak var sectionTitle: UIButton!
+    @IBOutlet weak var sectionDescription: UILabel!
+    @IBOutlet weak var descriptionStackView: UIStackView!
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static var nib: UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configureHeader(index : IndexPath) {
+        sectionTitle.setTitle(Path.allCases[index.section].message, for: .normal)
+        sectionDescription.text = "0개 상품이 등록되어 있습니다"
+    }
+    
+    @IBAction func titleButtonTouched(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            self.descriptionStackView.isHidden = !self.descriptionStackView.isHidden
+            self.layoutIfNeeded()
+        }
+    }
+}
