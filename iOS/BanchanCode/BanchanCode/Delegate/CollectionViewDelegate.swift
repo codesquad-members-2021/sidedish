8e88fd38eee72f1e1f2027b0e82dd3dbbd2c4035
1song2 <1song2918@gmail.com>
2021-04-22 11:08:53 +0900

    refactor : modify cell item size

--- iOS/BanchanCode/BanchanCode/Delegate/CollectionViewDelegate.swift
+++ iOS/BanchanCode/BanchanCode/Delegate/CollectionViewDelegate.swift
@@ -10,20 +10,12 @@ import UIKit
 class CollectionViewDelegate: NSObject, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        //회전에 따라 비율을 직접 설정해서 넣는 방법이 좋을 듯해서 넣어봤습니다.
-        if UIDevice.current.orientation.isLandscape {
-            return CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width * 0.2)
-        }else {
-            return CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width * 0.379)
-        }        
+        return CGSize(width: collectionView.frame.size.width - 32.0, height: 130.0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         let width: CGFloat = collectionView.frame.width
-        let height: CGFloat = 32
+        let height: CGFloat = 32.0
         return CGSize(width: width, height: height)
     }
-    
-    
 }
