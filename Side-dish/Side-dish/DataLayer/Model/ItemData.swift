9aca999fd946679160685c268e6c27eda8d702f1
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-04-22 21:56:08 +0900

    Add: 아이템 디테일 데이터모델 추가

---
+++ Side-dish/Side-dish/DataLayer/Model/ItemData.swift
@@ -0,0 +1,40 @@
+//
+//  ItemData.swift
+//  Side-dish
+//
+//  Created by 조중윤 on 2021/04/22.
+//
+
+import Foundation
+
+struct ItemData: Codable {
+    let topImage: String
+    let thumbImages: [String]
+    let productDescription, point, deliveryInfo, deliveryFee: String
+    let prices: [String]
+    let detailSection: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case topImage = "top_image"
+        case thumbImages = "thumb_images"
+        case productDescription = "product_description"
+        case point
+        case deliveryInfo = "delivery_info"
+        case deliveryFee = "delivery_fee"
+        case prices
+        case detailSection = "detail_section"
+    }
+    init() {
+        self.topImage = "test"
+        self.thumbImages = ["test"]
+        self.productDescription = "test"
+        self.point = "test"
+        self.deliveryInfo = "test"
+        self.deliveryFee = "test"
+        self.prices = []
+        self.detailSection = []
+    }
+}
+    
+    
+
---
+++ Side-dish/Side-dish/DataLayer/Model/ItemDetails.swift
@@ -0,0 +1,13 @@
+//
+//  ItemDetails.swift
+//  Side-dish
+//
+//  Created by 조중윤 on 2021/04/22.
+//
+
+import Foundation
+
+struct ItemDetails: Codable {
+    let hash: String
+    let data: ItemData
+}
