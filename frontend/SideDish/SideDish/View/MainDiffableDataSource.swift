9d0114ad2fc573c05225079946d28bc45045bdf4
BMO <b38238496@gmail.com>
2021-04-22 17:59:36 +0900

    fix: Solve the error in section

--- frontend/SideDish/SideDish/View/MainDiffableDataSource.swift
+++ frontend/SideDish/SideDish/View/MainDiffableDataSource.swift
@@ -4,7 +4,7 @@ import UIKit
 
 class MainDiffableDataSource {
     
-    enum sectionTitle: String {
+    enum sectionTitle: String, CaseIterable {
         case main = "모두가 좋아하는 든든한 메인요리"
         case soup = "정성이 담긴 뜨끈뜨끈 국물요리"
         case side = "식탁을 풍성하게 하는 정갈한 밑반찬"
@@ -21,36 +21,29 @@ class MainDiffableDataSource {
             return cell
         }
         
-        self.dataSource.supplementaryViewProvider = setupHeader(collectionView: kind: indexPath:)
+        setHeaderSnapShot()
     }
     
-    func applySnapshot(main: [Menu], soup: [Menu], side: [Menu]) {
-        var snapshot = NSDiffableDataSourceSnapshot<sectionTitle, Menu>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(main, toSection: .main)
-        snapshot.appendSections([.soup])
-        snapshot.appendItems(soup, toSection: .soup)
-        snapshot.appendSections([.side])
-        snapshot.appendItems(side, toSection: .side)
+    func setHeaderSnapShot() {
+        self.dataSource.supplementaryViewProvider = setupHeader(collectionView: kind: indexPath:)
 
+        var snapshot = self.dataSource.snapshot()
+        snapshot.appendSections(sectionTitle.allCases)
+        self.dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    func applySnapshot(menu:[Menu], section: sectionTitle) {
+        var snapshot = self.dataSource.snapshot()
+        snapshot.appendItems(menu, toSection: section)
+        
         self.dataSource.apply(snapshot, animatingDifferences: true)
     }
     
     func setupHeader(collectionView: UICollectionView, kind: String, indexPath: IndexPath) -> UICollectionReusableView? {
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerView", for: indexPath) as! HeaderView
         
-        header.configureButtonName(text: matchTitle(sectionNumber: indexPath.section))
+        let section = self.dataSource.snapshot().sectionIdentifiers[indexPath.section]
+        header.configureButtonName(text: section.rawValue)
         return header
-    }
-    
-    func matchTitle(sectionNumber: Int) -> String {
-        switch sectionNumber {
-        case 0:
-            return sectionTitle.main.rawValue
-        case 1:
-            return sectionTitle.soup.rawValue
-        default:
-            return sectionTitle.side.rawValue
-        }
     }
 }
--- frontend/SideDish/SideDish/ViewController.swift
+++ frontend/SideDish/SideDish/ViewController.swift
@@ -31,7 +31,6 @@ class ViewController: UIViewController {
         sideDishCollectionView.register(nibName, forCellWithReuseIdentifier: "menuCell")
         
         let nibHeaderName = UINib(nibName: "HeaderView", bundle: .none)
-//        sideDishCollectionView.register(nibHeaderName, forCellWithReuseIdentifier: "headerView")
         sideDishCollectionView.register(nibHeaderName, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "headerView")
         
         self.dataSource.setupDataSource(collectionView: self.sideDishCollectionView)
@@ -39,49 +38,42 @@ class ViewController: UIViewController {
     
     private func loadMain() {
         DataTaskManager.get(url: .main, completion: { (result) in
-            DispatchQueue.global().async {
-                switch result {
-                case .success(let data):
-                    self.mainMenus.add(menuList: data.body)
-                    self.putData()
-                case.failure(let error):
-                    print(error.localizedDescription)
-                }
+            switch result {
+            case .success(let data):
+                self.mainMenus.add(menuList: data.body)
+                self.putData(menu: self.mainMenus.giveMenu(), section: .main)
+            case.failure(let error):
+                print(error.localizedDescription)
             }
         })
     }
     
     private func loadSoup() {
         DataTaskManager.get(url: .soup, completion: { (result) in
-            DispatchQueue.global().async {
-                switch result {
-                case .success(let data):
-                    self.soupMenus.add(menuList: data.body)
-                    self.putData()
-                case.failure(let error):
-                    print(error.localizedDescription)
-                }
+            switch result {
+            case .success(let data):
+                self.soupMenus.add(menuList: data.body)
+                self.putData(menu: self.soupMenus.giveMenu(), section: .soup)
+            case.failure(let error):
+                print(error.localizedDescription)
             }
         })
     }
     
     private func loadSide() {
         DataTaskManager.get(url: .side, completion: { (result) in
-            DispatchQueue.global().async {
-                switch result {
-                case .success(let data):
-                    self.sideMenus.add(menuList: data.body)
-                    self.putData()
-                case.failure(let error):
-                    print(error.localizedDescription)
-                }
+            switch result {
+            case .success(let data):
+                self.sideMenus.add(menuList: data.body)
+                self.putData(menu: self.sideMenus.giveMenu(), section: .side)
+            case.failure(let error):
+                print(error.localizedDescription)
             }
         })
     }
     
-    private func putData() {
-        guard let main = mainMenus.giveMenu(), let soup = soupMenus.giveMenu(), let side = sideMenus.giveMenu() else { return }
-        self.dataSource.applySnapshot(main: main, soup: soup, side: side)
+    private func putData(menu: [Menu]?, section: MainDiffableDataSource.sectionTitle) {
+        guard let main = menu else { return }
+        self.dataSource.applySnapshot(menu: main, section: section)
     }
-
 }
