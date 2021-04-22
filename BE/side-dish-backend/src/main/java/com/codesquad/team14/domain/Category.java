89687b8611137aa0b008cd47651bb36d82245127
DongHunKim <ultraraptor@naver.com>
2021-04-22 14:55:00 +0900

    feat: 카테고리 도메인 생성
    1:N 관계를 위해 아이템은 셋으로 지정

--- BE/side-dish-backend/src/main/java/com/codesquad/team14/domain/Category.java
+++ BE/side-dish-backend/src/main/java/com/codesquad/team14/domain/Category.java
@@ -1,4 +1,40 @@
 package com.codesquad.team14.domain;
 
+import org.springframework.data.annotation.Id;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Category {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Set<Item> items = new HashSet<>();
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
 }
