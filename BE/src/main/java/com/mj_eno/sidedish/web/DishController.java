162df38827cd7bbce81bd42d58d21b04b2815c5d
jeong-inho <derosatam76@gmail.com>
2021-04-23 02:16:14 +0900

    Feat: DishController 클래스에 메소드 생성
    Dish 관련 요청을 처리하기 위해 DishController 생성, 모든 best 메뉴 요청에 대한 viewAllBestDishes, 카테고리 별 best 메뉴 요청에 대한 viewBestDishesByCategory, 카테고리 별 메뉴 요청에 대한 viewDishes, 메뉴의 상세 정보 요청에 대한 viewDetailDish, 랜덤 메뉴 요청에 대한 viewRecommendDish, 주문에 대한 orderDish 메소드 생성

--- BE/src/main/java/com/mj_eno/sidedish/web/DishController.java
+++ BE/src/main/java/com/mj_eno/sidedish/web/DishController.java
@@ -1,11 +1,10 @@
 package com.mj_eno.sidedish.web;
 
 import com.mj_eno.sidedish.service.DishService;
-import com.mj_eno.sidedish.web.dto.MainDishResponseDTO;
+import com.mj_eno.sidedish.web.dto.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +18,39 @@ public class DishController {
         this.dishService = dishService;
     }
 
-    @GetMapping("/main")
-    public List<MainDishResponseDTO> viewBestDishes() {
-        logger.info("모든 메인 반찬 요청");
-        return dishService.findAllMainDish();
+    @GetMapping("/best")
+    public List<BestDishResponseDTO> viewAllBestDishes() {
+        logger.info("모든 best 메뉴 요청");
+        return dishService.findAllBestDish();
+    }
+
+    @GetMapping("/best/{categoryId}")
+    public BestDishResponseDTO viewBestDishesByCategory(@PathVariable Long categoryId) {
+        logger.info(categoryId + "번 카테고리의 best 메뉴 요청");
+        return dishService.findAllBestDishByCategoryIdAndRandom(categoryId);
+    }
+
+    @GetMapping("/{category}")
+    public List<DishResponseDTO> viewDishes(@PathVariable String category) {
+        logger.info("모든 " + category + " 메뉴 요청");
+        return dishService.findAllDishByCategory(category);
+    }
+
+    @GetMapping("/detail/{hash}")
+    public DetailDishResponseDTO viewDetailDish(@PathVariable String hash) {
+        logger.info(hash + " 값에 해당하는 메뉴의 상세 정보 요청");
+        return dishService.getDetailMenu(hash);
+    }
+
+    @GetMapping("/recommend/{limit}")
+    public List<DishResponseDTO> viewRecommendDish(@PathVariable int limit) {
+        logger.info(limit + "개의 랜덤 메뉴 요청");
+        return dishService.findDishByRandomLimit(limit);
+    }
+
+    @PatchMapping("/order")
+    public OrderDishResponseDTO orderDish(@RequestBody OrderDishRequestDTO requestDTO) {
+        logger.info("주문 요청");
+        return dishService.orderDish(requestDTO);
     }
 }
