c3ebdb0f27d4df09605016984c7544f03c7b9543
ellyheetov <yaaajcm@naver.com>
2021-04-22 23:33:02 +0900

    fix typo: apply the NotoSansKR font

--- SideDish/SideDish/Extension/ImageView+load.swift
+++ SideDish/SideDish/Extension/ImageView+load.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 extension UIImageView {
-    func load(url: URL?) {
+    func load(url: URL?, completion : @escaping () -> ()) {
         guard let url = url else { return }
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url) {
                 if let image = UIImage(data: data) {
                     DispatchQueue.main.async {
                         self?.image = image
+                        completion()
                     }
                 }
             }
--- SideDish/SideDish/View/CardCell.swift
+++ SideDish/SideDish/View/CardCell.swift
@@ -15,32 +15,61 @@ class CardCell: UICollectionViewCell {
     @IBOutlet weak var detail: UILabel!
     @IBOutlet weak var discountPrice: UILabel!
     @IBOutlet weak var originalPrice: UILabel!
-    @IBOutlet weak var eventBadge: UILabel!
-    @IBOutlet weak var launchingBadge: UILabel!
-    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     @IBOutlet weak var badgeView: UIStackView!
     
     var card : Card? {
         didSet {
-            thumbnail.load(url: card?.imageURL)
+            thumbnail.load(url: card?.imageURL, completion: {
+                self.activityIndicator.stopAnimating()
+            })
             title.text = card?.title
             detail.text = card?.detail
             discountPrice.text = card?.discountPrice?.description
-            originalPrice.text = card?.originalPrice?.description
-            
+            originalPrice.attributedText = NSMutableAttributedString(string: card?.originalPrice?.description ?? "",
+                                                                          attributes: [NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue])
             guard let badges = card?.badge else {
                 badgeView.isHidden = true
                 return
+            }
+            
+            badges.forEach{ badge in
+                let view = makeBadge(with: badge)
+                self.badgeView.addArrangedSubview(view)
             }
         }
     }
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        self.activityIndicator.startAnimating()
         configureTitle()
         configureDetail()
         configureThumbnail()
-        configureBedge()
+        configureBadgeView()
+    }
+    func makeBadge(with name: String) -> UIView {
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 72, height: 25))
+        
+        view.backgroundColor = .white
+        view.layer.backgroundColor = #colorLiteral(red: 0.5095996261, green: 0.8290402293, blue: 0.1742436588, alpha: 1)
+        view.layer.cornerRadius = 5
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 56, height: 17))
+        label.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        label.font = UIFont(name: "NotoSansKR-Bold", size: 12)
+        label.text = name
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.widthAnchor.constraint(equalToConstant: 56).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 17).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 4).isActive = true
+     
+        return view
     }
     func configureTitle(){
         self.title.textColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
@@ -62,15 +91,7 @@ class CardCell: UICollectionViewCell {
     func configureThumbnail(){
         self.thumbnail.layer.cornerRadius = 5
     }
-    func configureBedge(){
-        self.eventBadge.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-        self.eventBadge.font = UIFont(name: "NotoSansKR-Bold", size: 14)
-        self.eventBadge.backgroundColor = #colorLiteral(red: 0.5095996261, green: 0.8290402293, blue: 0.1742436588, alpha: 1)
-        self.eventBadge.layer.cornerRadius = 5
-        
-        self.launchingBadge.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-        self.launchingBadge.font = UIFont(name: "NotoSansKR-Bold", size: 14)
-        self.launchingBadge.backgroundColor =  #colorLiteral(red: 0.6011084318, green: 0.8016367555, blue: 0.9797580838, alpha: 1)
-        self.launchingBadge.layer.cornerRadius = 5
+    func configureBadgeView(){
+        self.badgeView.translatesAutoresizingMaskIntoConstraints = false
     }
 }
