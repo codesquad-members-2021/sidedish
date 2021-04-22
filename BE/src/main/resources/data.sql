810bb8987881503a2c24c7c8eb499dce075d4bdb
MJbae <gently.gogo@gmail.com>
2021-04-22 11:59:09 +0900

    feat: menu_category 별로 dish 생성

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,61 +1,108 @@
---best_menu_category
+-- -----------------------------------------------------
+-- best_menu_category
+-- -----------------------------------------------------
 insert into best_menu_category(name) values ('할인특가 세트상품');
 insert into best_menu_category(name) values ('풍성한 고기반찬');
 insert into best_menu_category(name) values ('편리한 반찬세트');
 insert into best_menu_category(name) values ('간편한 덮밥요리');
 insert into best_menu_category(name) values ('우리아이 영양반찬');
 
---menu_category
+-- -----------------------------------------------------
+-- menu_category
+-- -----------------------------------------------------
+insert into menu_category(name) values ('best');
 insert into menu_category(name) values ('main');
 insert into menu_category(name) values ('soup');
 insert into menu_category(name) values ('side');
 
-
---/best/1
+-- -----------------------------------------------------
+-- best/1
+-- -----------------------------------------------------
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H9881', '[소중한식사] 경상도 한상차림', '[소중한식사] 경상도 한상차림','경상도 명물 요리 세 가지를 한 상에!', 39000, 31200, 123, 1, 1);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HDF4C', '[딩고] 시그니처 3종 SET', '[딩고] 시그니처 3종 SET','오늘 저녁은 이상하게 안주빨을 세워보고 싶다!', 32800, 24000, 123, 2, 1);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H7F20', '[집밥의완성]초여름보양세트', '[집밥의완성]초여름보양세트','무더위에 대비하는 6월 한상차림 초여름 보양세트', 26000, 18000, 123, 3, 1);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('E055F', '[동네부엌] 사계절 건강식단 작은상', '[동네부엌] 사계절 건강식단 작은상','마포엄마들이 사랑해온 건강반찬', 24000, 10000, 123, 1, 4);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('FDAEB', '[집밥의완성] 한상 가득 세트 (3~4인)', '[집밥의완성] 한상 가득 세트 (3~4인)','[집밥의완성] 한상 가득 세트 (3~4인)', 40000, 31000, 100, 2, 1);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H762E', '[옹가솜씨] 한끼뚝딱세트', '[옹가솜씨] 한끼뚝딱세트','한 세트만 있으면 한 끼 뚝딱', 19000, 15000, 123, 3, 1);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HDF4C', '[딩고] 시그니처 3종 SET', '[딩고] 시그니처 3종 SET','오늘 저녁은 이상하게 안주빨을 세워보고 싶다!', 32800, 24000, 123, 1, 1);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H7F20', '[집밥의완성]초여름보양세트', '[집밥의완성]초여름보양세트','무더위에 대비하는 6월 한상차림 초여름 보양세트', 26000, 18000, 123, 1, 1);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('E055F', '[동네부엌] 사계절 건강식단 작은상', '[동네부엌] 사계절 건강식단 작은상','마포엄마들이 사랑해온 건강반찬', 24000, 10000, 123, 1, 1);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('FDAEB', '[집밥의완성] 한상 가득 세트 (3~4인)', '[집밥의완성] 한상 가득 세트 (3~4인)','[집밥의완성] 한상 가득 세트 (3~4인)', 40000, 31000, 100, 1, 1);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H762E', '[옹가솜씨] 한끼뚝딱세트', '[옹가솜씨] 한끼뚝딱세트','한 세트만 있으면 한 끼 뚝딱', 19000, 15000, 123, 1, 1);
 
---/best/2
+-- -----------------------------------------------------
+-- best/2
+-- -----------------------------------------------------
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HA567', '[아내의쉐프] 아쉐찜닭', '[아내의쉐프] 아쉐찜닭','[아내의쉐프] 아쉐찜닭', 11000, 10000, 123, 1, 2);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H206E', '[애슐리투고] 로스트 콤보 450g', '[애슐리투고] 로스트 콤보 450g','립과 치킨이 만나  더욱 맛있게', 12000, 10000, 100, 2, 2);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H75A2', '[한옥집]소갈비김치찜 2인분', '[한옥집]소갈비김치찜 2인분','부드러운 소갈비와 함께 푹 쪄낸 김치찜!', 16000, 15000, 90, 3, 2);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H206E', '[애슐리투고] 로스트 콤보 450g', '[애슐리투고] 로스트 콤보 450g','립과 치킨이 만나  더욱 맛있게', 12000, 10000, 100, 1, 2);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H75A2', '[한옥집]소갈비김치찜 2인분', '[한옥집]소갈비김치찜 2인분','부드러운 소갈비와 함께 푹 쪄낸 김치찜!', 16000, 15000, 90, 1, 2);
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H206E', '[남도애꽃] 통오징어초롱 300g', '[남도애꽃] 통오징어초롱 300g','국내산 오징어를 통째로 불맛소스와 함께', 14000, 10000, 123, 1, 2);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HA8B9', '[황태종가] 황태양념구이 300g', '[황태종가] 황태양념구이 300g','청정 황태마을 인제에서 온 깊은 맛', 15000, 11000, 100, 2, 2);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HA8B9', '[황태종가] 황태양념구이 300g', '[황태종가] 황태양념구이 300g','청정 황태마을 인제에서 온 깊은 맛', 15000, 11000, 100, 1, 2);
 
---/best/3
+-- -----------------------------------------------------
+-- best/3
+-- -----------------------------------------------------
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H206E', '[남도애꽃] 통오징어초롱 300g', '[남도애꽃] 통오징어초롱 300g','국내산 오징어를 통째로 불맛소스와 함께', 14000, 10000, 123, 1, 3);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HA8B9', '[황태종가] 황태양념구이 300g', '[황태종가] 황태양념구이 300g','청정 황태마을 인제에서 온 깊은 맛', 15000, 11000, 100, 2, 3);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HA8B9', '[황태종가] 황태양념구이 300g', '[황태종가] 황태양념구이 300g','청정 황태마을 인제에서 온 깊은 맛', 15000, 11000, 100, 1, 3);
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HA567', '[아내의쉐프] 아쉐찜닭', '[아내의쉐프] 아쉐찜닭','[아내의쉐프] 아쉐찜닭', 11000, 10000, 123, 1, 3);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H206E', '[애슐리투고] 로스트 콤보 450g', '[애슐리투고] 로스트 콤보 450g','립과 치킨이 만나  더욱 맛있게', 12000, 10000, 100, 2, 3);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H75A2', '[한옥집]소갈비김치찜 2인분', '[한옥집]소갈비김치찜 2인분','부드러운 소갈비와 함께 푹 쪄낸 김치찜!', 16000, 15000, 90, 3, 3);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H206E', '[애슐리투고] 로스트 콤보 450g', '[애슐리투고] 로스트 콤보 450g','립과 치킨이 만나  더욱 맛있게', 12000, 10000, 100, 1, 3);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H75A2', '[한옥집]소갈비김치찜 2인분', '[한옥집]소갈비김치찜 2인분','부드러운 소갈비와 함께 푹 쪄낸 김치찜!', 16000, 15000, 90, 1, 3);
 
---/best/4
+-- -----------------------------------------------------
+-- best/4
+-- -----------------------------------------------------
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('E055F', '[동네부엌] 사계절 건강식단 작은상', '[동네부엌] 사계절 건강식단 작은상','마포엄마들이 사랑해온 건강반찬', 24000, 10000, 123, 1, 4);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('FDAEB', '[집밥의완성] 한상 가득 세트 (3~4인)', '[집밥의완성] 한상 가득 세트 (3~4인)','[집밥의완성] 한상 가득 세트 (3~4인)', 40000, 31000, 100, 2, 4);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H762E', '[옹가솜씨] 한끼뚝딱세트', '[옹가솜씨] 한끼뚝딱세트','한 세트만 있으면 한 끼 뚝딱', 19000, 15000, 123, 3, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('FDAEB', '[집밥의완성] 한상 가득 세트 (3~4인)', '[집밥의완성] 한상 가득 세트 (3~4인)','[집밥의완성] 한상 가득 세트 (3~4인)', 40000, 31000, 100, 1, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H762E', '[옹가솜씨] 한끼뚝딱세트', '[옹가솜씨] 한끼뚝딱세트','한 세트만 있으면 한 끼 뚝딱', 19000, 15000, 123, 1, 4);
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H9881', '[소중한식사] 경상도 한상차림', '[소중한식사] 경상도 한상차림','경상도 명물 요리 세 가지를 한 상에!', 39000, 31200, 123, 1, 4);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HDF4C', '[딩고] 시그니처 3종 SET', '[딩고] 시그니처 3종 SET','오늘 저녁은 이상하게 안주빨을 세워보고 싶다!', 32800, 24000, 123, 2, 4);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H7F20', '[집밥의완성]초여름보양세트', '[집밥의완성]초여름보양세트','무더위에 대비하는 6월 한상차림 초여름 보양세트', 26000, 18000, 123, 3, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HDF4C', '[딩고] 시그니처 3종 SET', '[딩고] 시그니처 3종 SET','오늘 저녁은 이상하게 안주빨을 세워보고 싶다!', 32800, 24000, 123, 1, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H7F20', '[집밥의완성]초여름보양세트', '[집밥의완성]초여름보양세트','무더위에 대비하는 6월 한상차림 초여름 보양세트', 26000, 18000, 123, 1, 4);
 
---/best/5
+-- -----------------------------------------------------
+-- best/5
+-- -----------------------------------------------------
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H05FB', '[옹가솜씨] 소고기쩜짜장 280g', '[옹가솜씨] 소고기쩜짜장 280g','짜장의 착한 변신', 8000, 5000, 123, 1, 5);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H0699', '[무명식당]뿌리채소비빔밥 2인분', '[무명식당]뿌리채소비빔밥 2인분','각종 뿌리채소를 듬뿍 넣어 지은 뿌리채소영양밥', 40000, 31000, 100, 2, 5);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HBDEF', '[미노리키친] 규동 250g', '[미노리키친] 규동 250g','일본인의 소울푸드! 한국인도 좋아하는 소고기덮밥', 19000, 15000, 123, 3, 5);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H0699', '[무명식당]뿌리채소비빔밥 2인분', '[무명식당]뿌리채소비빔밥 2인분','각종 뿌리채소를 듬뿍 넣어 지은 뿌리채소영양밥', 40000, 31000, 100, 1, 5);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HBDEF', '[미노리키친] 규동 250g', '[미노리키친] 규동 250g','일본인의 소울푸드! 한국인도 좋아하는 소고기덮밥', 19000, 15000, 123, 1, 5);
 insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H9881', '[소중한식사] 경상도 한상차림', '[소중한식사] 경상도 한상차림','경상도 명물 요리 세 가지를 한 상에!', 39000, 31200, 123, 1, 5);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HDF4C', '[딩고] 시그니처 3종 SET', '[딩고] 시그니처 3종 SET','오늘 저녁은 이상하게 안주빨을 세워보고 싶다!', 32800, 24000, 123, 2, 5);
-insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H7F20', '[집밥의완성]초여름보양세트', '[집밥의완성]초여름보양세트','무더위에 대비하는 6월 한상차림 초여름 보양세트', 26000, 18000, 123, 3, 5);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('HDF4C', '[딩고] 시그니처 3종 SET', '[딩고] 시그니처 3종 SET','오늘 저녁은 이상하게 안주빨을 세워보고 싶다!', 32800, 24000, 123, 1, 5);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id, best_menu_category_id) values ('H7F20', '[집밥의완성]초여름보양세트', '[집밥의완성]초여름보양세트','무더위에 대비하는 6월 한상차림 초여름 보양세트', 26000, 18000, 123, 1, 5);
 
+-- -----------------------------------------------------
+-- main
+-- -----------------------------------------------------
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HBDEF', '[미노리키친] 규동 250g', '[미노리키친] 규동 250g','일본인의 소울푸드! 한국인도 좋아하는 소고기덮밥', 6500, 6000, 50, 2);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HDF73', '[빅마마의밥친구] 아삭 고소한 연근고기조림 250g', '[빅마마의밥친구] 아삭 고소한 연근고기조림 250g','편식하는 아이도 좋아하는 건강한 연근조림', 5500, 5000, 50, 2);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HBDEF', '[미노리키친] 규동 250g', '[미노리키친] 규동 250g','일본인의 소울푸드! 한국인도 좋아하는 소고기덮밥', 6500, 6000, 50, 2);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HDF73', '[빅마마의밥친구] 아삭 고소한 연근고기조림 250g', '[빅마마의밥친구] 아삭 고소한 연근고기조림 250g','편식하는 아이도 좋아하는 건강한 연근조림', 5500, 5000, 50, 2);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HBDEF', '[미노리키친] 규동 250g', '[미노리키친] 규동 250g','일본인의 소울푸드! 한국인도 좋아하는 소고기덮밥', 6500, 6000, 50, 2);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HDF73', '[빅마마의밥친구] 아삭 고소한 연근고기조림 250g', '[빅마마의밥친구] 아삭 고소한 연근고기조림 250g','편식하는 아이도 좋아하는 건강한 연근조림', 5500, 5000, 50, 2);
 
---badge
+-- -----------------------------------------------------
+-- soup
+-- -----------------------------------------------------
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('H72C3', '[수하동] 특곰탕 850g', '[수하동] 특곰탕 850g','100% 한우양지로 끓여낸 70년전통의 서울식곰탕', 15000, 14000, 100, 3);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HA6EE', '[빅마마의밥친구] 된장찌개 900g', '[빅마마의밥친구] 된장찌개 900g','항아리에서 숙성시킨 집된장으로만 맛을내 짜지 않은 된장찌개', 10000, 8000, 50, 3);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('H72C3', '[수하동] 특곰탕 850g', '[수하동] 특곰탕 850g','100% 한우양지로 끓여낸 70년전통의 서울식곰탕', 15000, 14000, 100, 3);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HA6EE', '[빅마마의밥친구] 된장찌개 900g', '[빅마마의밥친구] 된장찌개 900g','항아리에서 숙성시킨 집된장으로만 맛을내 짜지 않은 된장찌개', 10000, 8000, 50, 3);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('H72C3', '[수하동] 특곰탕 850g', '[수하동] 특곰탕 850g','100% 한우양지로 끓여낸 70년전통의 서울식곰탕', 15000, 14000, 100, 3);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HA6EE', '[빅마마의밥친구] 된장찌개 900g', '[빅마마의밥친구] 된장찌개 900g','항아리에서 숙성시킨 집된장으로만 맛을내 짜지 않은 된장찌개', 10000, 8000, 50, 3);
+
+-- -----------------------------------------------------
+-- side
+-- -----------------------------------------------------
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HBBCC', '[마샐미디쉬] 유자소스 연근무침 250g', '[마샐미디쉬] 유자소스 연근무침 250g','향긋한 유자향과 아삭한 연근', 6000, 5000, 10, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('H1939', '[동네부엌] 쇠고기야채장조림 200g', '[동네부엌] 쇠고기야채장조림 200g','부드러운 고기부터 밥비벼먹기 딱좋은 국물까지', 8000, 7000, 15, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HBBCC', '[마샐미디쉬] 유자소스 연근무침 250g', '[마샐미디쉬] 유자소스 연근무침 250g','향긋한 유자향과 아삭한 연근', 6000, 5000, 10, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('H1939', '[동네부엌] 쇠고기야채장조림 200g', '[동네부엌] 쇠고기야채장조림 200g','부드러운 고기부터 밥비벼먹기 딱좋은 국물까지', 8000, 7000, 15, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('HBBCC', '[마샐미디쉬] 유자소스 연근무침 250g', '[마샐미디쉬] 유자소스 연근무침 250g','향긋한 유자향과 아삭한 연근', 6000, 5000, 10, 4);
+insert into dish(`hash`, alt, title, description, n_price, s_price, `point`, menu_category_id) values ('H1939', '[동네부엌] 쇠고기야채장조림 200g', '[동네부엌] 쇠고기야채장조림 200g','부드러운 고기부터 밥비벼먹기 딱좋은 국물까지', 8000, 7000, 15, 4);
+
+-- -----------------------------------------------------
+-- badge
+-- -----------------------------------------------------
 insert into badge(name) values ('이벤트 특가');
 insert into badge(name) values ('베스트');
 insert into badge(name) values ('론칭 특가');
 
---dish_badge_best
+-- -----------------------------------------------------
+-- dish_badge_best
+-- -----------------------------------------------------
 insert into dish_badge(dish_id, badge_id) values (1, 2);
 insert into dish_badge(dish_id, badge_id) values (2, 2);
 insert into dish_badge(dish_id, badge_id) values (3, 2);
@@ -85,7 +132,9 @@ insert into dish_badge(dish_id, badge_id) values (26, 2);
 insert into dish_badge(dish_id, badge_id) values (27, 2);
 insert into dish_badge(dish_id, badge_id) values (28, 2);
 
---dish_badge_event_sale
+-- -----------------------------------------------------
+-- dish_badge_event_sale
+-- -----------------------------------------------------
 insert into dish_badge(dish_id, badge_id) values (13, 1);
 insert into dish_badge(dish_id, badge_id) values (14, 1);
 insert into dish_badge(dish_id, badge_id) values (15, 1);
@@ -98,7 +147,9 @@ insert into dish_badge(dish_id, badge_id) values (21, 3);
 insert into dish_badge(dish_id, badge_id) values (22, 1);
 insert into dish_badge(dish_id, badge_id) values (23, 3);
 
---image
+-- -----------------------------------------------------
+-- image
+-- -----------------------------------------------------
 insert into image(url, top, dish_id) values ('https://public.codesquad.kr/jk/storeapp/data/2d3f99a9a35601f4e98837bc4d39b2c8.jpg', 1, 1);
 insert into image(url, top, dish_id) values ('https://public.codesquad.kr/jk/storeapp/data/2d3f99a9a35601f4e98837bc4d39b2c8.jpg', 1, 2);
 insert into image(url, top, dish_id) values ('https://public.codesquad.kr/jk/storeapp/data/2d3f99a9a35601f4e98837bc4d39b2c8.jpg', 1, 3);
