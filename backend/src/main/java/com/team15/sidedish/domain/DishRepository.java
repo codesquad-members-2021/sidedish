d1532bcbee8310e559c790cb426d4b919c4f648d
woody <woojihye2339@gmail.com>
2021-04-22 11:58:13 +0900

    [#17] feature: Add custom query at DishRepository
    - DishRepository::findById, DishRepository::findAll 메소드를 오버라이드
      - 커스텀 쿼리를 사용하여, 클라이언트의 요청에 맞는 데이터(ItemDTO)를 추출

--- backend/src/main/java/com/team15/sidedish/domain/DishRepository.java
+++ backend/src/main/java/com/team15/sidedish/domain/DishRepository.java
@@ -7,7 +7,14 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface DishRepository extends CrudRepository<Dish, String> {
-    Optional<Dish> findById(String hash);
-    //List<Dish> findAllBySectionName(String sectionName);
+public interface DishRepository extends CrudRepository<ItemDTO, String> {
+
+    @Query("SELECT di.`hash` AS `detail_hash`, di.`top_image` AS `image`, di.`title` AS `alt`, de.`delivery_type`, di.`title`, di.`description`, di.`n_price`, di.`s_price`, e.`badge` from dish di JOIN delivery de on di.`hash` = de.`dish_hash` LEFT OUTER JOIN `event` e on e.`dish_hash` = di.`hash` where e.`dish_hash`=:hash")
+    @Override
+    Optional<ItemDTO> findById(String hash);
+
+    @Query("select di.`hash` AS `detail_hash`, di.`top_image` AS `image`, di.`title` AS `alt`, de.`delivery_type`, di.`title`, di.`description`, di.`n_price`, di.`s_price`, e.`badge` from dish di JOIN delivery de on di.`hash` = de.`dish_hash` LEFT OUTER JOIN `event` e on e.`dish_hash` = di.`hash`")
+    @Override
+    List<ItemDTO> findAll();
+
 }
