cd6651673b6658dbbc9c9af8a0eddc8d3abb2d60
jeong-inho <derosatam76@gmail.com>
2021-04-23 02:03:59 +0900

    Feat: DishRepository 인터페이스에 메소드 생성
    DishRepository 인터페이스에 hash 값으로 찾는 findByHash 메소드, 베스트 메뉴를 카테고리로 찾아 랜덤으로 3개 가져오는 findByBestMenuCategoryIdAndRandomAndLimit 메소드, limit 제한을 받아 랜덤으로 찾는 findByRandomAndLimit 메소드 생성

--- BE/src/main/java/com/mj_eno/sidedish/domain/dish/DishRepository.java
+++ BE/src/main/java/com/mj_eno/sidedish/domain/dish/DishRepository.java
@@ -1,10 +1,20 @@
 package com.mj_eno.sidedish.domain.dish;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DishRepository extends CrudRepository<Dish, Long> {
 
+    Optional<Dish> findByHash(String hash);
+
     List<Dish> findAllByMenuCategoryId(Long menuCategoryId);
+
+    @Query("SELECT * FROM dish WHERE dish.best_menu_category_id = :bestMenuCategoryId ORDER BY rand() LIMIT 3;")
+    List<Dish> findByBestMenuCategoryIdAndRandomAndLimit(Long bestMenuCategoryId);
+
+    @Query("SELECT * FROM dish ORDER BY rand() LIMIT :limit;")
+    List<Dish> findByRandomAndLimit(int limit);
 }
