55d0e10f802524ef1de9723004b2e4c562ead5f6
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-04-22 14:41:14 +0900

    [ADD] : Json파일을 읽어내어 Main 반찬을 생성하는 기능 추가
    ClassPathResource를 이용하여 Json파일을 읽어, Main 반찬을 List로 생성한다.

--- be/sidedish-be/src/main/java/com/codesquad/sidedish/utils/SampleDataFactory.java
+++ be/sidedish-be/src/main/java/com/codesquad/sidedish/utils/SampleDataFactory.java
@@ -1,5 +1,6 @@
 package com.codesquad.sidedish.utils;
 
+import com.codesquad.sidedish.web.sidedish.ItemDTO;
 import com.codesquad.sidedish.web.sidedish.SidedishDTO;
 import org.springframework.core.io.ClassPathResource;
 
@@ -10,6 +11,14 @@ public class SampleDataFactory {
     public static List<SidedishDTO> createBestMenus() {
         try {
             return SidedishDTO.listFrom(new ClassPathResource("sample-data/best-sidedishes.json").getFile());
+        } catch (IOException e) {
+            throw new IllegalStateException("경로가 올바르지 않습니다.");
+        }
+    }
+
+    public static List<ItemDTO> createMainMenus() {
+        try {
+            return ItemDTO.listForm(new ClassPathResource("sample-data/main-sidedishes.json").getFile());
         } catch (IOException e) {
             throw new IllegalStateException("경로가 올바르지 않습니다.");
         }
