47be5554ca9ed3cec0c9603aa6861850f9f7e601
ellyheetov <yaaajcm@naver.com>
2021-04-22 16:34:45 +0900

    refactor: divide file EndPoint, CardAPI

--- SideDish/SideDish/Network/CardAPI.swift
+++ SideDish/SideDish/Network/CardAPI.swift
@@ -9,16 +9,6 @@ import Alamofire
 import Foundation
 import Combine
 
-enum EndPoint : String, CodingKey {
-    case main
-    case soup
-    case side
-    
-    static let baseURL = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/"
-    var path : String { self.stringValue }
-    var url : URL { URL(string: EndPoint.baseURL + path)! }
-}
-
 class CardAPI {
 
     static func loadDish(type : EndPoint) -> AnyPublisher<CardsResponse, APIError> {
--- SideDish/SideDish/Network/EndPoint.swift
+++ SideDish/SideDish/Network/EndPoint.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+enum EndPoint : String, CodingKey {
+    case main
+    case soup
+    case side
+    
+    static let baseURL = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/"
+    var path : String { self.stringValue }
+    var url : URL { URL(string: EndPoint.baseURL + path)! }
+}
