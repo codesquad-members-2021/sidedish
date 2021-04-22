45c6b87890aa2ce9e0e5c24adf830927c6f92eb5
Ray suh <readingod@naver.com>
2021-04-22 09:01:33 +0900

    feat::hamster: Add Pedding View

--- iOS/sideDish/sideDish/View/EventInfoStackView.swift
+++ iOS/sideDish/sideDish/View/EventInfoStackView.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class EventInfoStackView: UIStackView {
     
+    var eventPriceLabel: EventInfoBadge?
+    var launchingPriceLabel: EventInfoBadge?
+    private var peddingView: UIView?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupMainView()
@@ -11,26 +15,14 @@ class EventInfoStackView: UIStackView {
         super.init(coder: coder)
         setupMainView()
     }
-    
-    func launchingPriceLabel(label text: String) {
-        let label = EventInfoBadge(frame: .zero)
-        label.text = text
-        label.backgroundColor = UIColor(red: 134/255, green: 198/255, blue: 255/255, alpha: 1)
-        addArrangedSubview(label)
-    }
-    
-    func eventPriceLabel(label text: String) {
-        let label = EventInfoBadge(frame: .zero)
-        label.text = text
-        label.backgroundColor = UIColor(red: 130/255, green: 211/255, blue: 45/255, alpha: 1)
-        addArrangedSubview(label)
-    }
 }
 
 extension EventInfoStackView {
     
     private func setupMainView() {
         configureMainView()
+        configureEventPriceLabel()
+        configureLaunchingPriceLabel()
     }
     
     private func configureMainView() {
@@ -38,5 +30,23 @@ extension EventInfoStackView {
         spacing = 4
         translatesAutoresizingMaskIntoConstraints = false
         heightAnchor.constraint(equalToConstant: 25).isActive = true
+    }
+    
+    private func configureEventPriceLabel() {
+        eventPriceLabel = EventInfoBadge(frame: .zero)
+        eventPriceLabel?.backgroundColor = UIColor(red: 130/255, green: 211/255, blue: 45/255, alpha: 1)
+        guard let priceLabel = eventPriceLabel else { return }
+        addArrangedSubview(priceLabel)
+        peddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 25))
+        addArrangedSubview(peddingView!)
+    }
+    
+    private func configureLaunchingPriceLabel() {
+        launchingPriceLabel = EventInfoBadge(frame: .zero)
+        launchingPriceLabel?.backgroundColor = UIColor(red: 134/255, green: 198/255, blue: 255/255, alpha: 1)
+        guard let priceLabel = launchingPriceLabel else { return }
+        addArrangedSubview(priceLabel)
+        peddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 25))
+        addArrangedSubview(peddingView!)
     }
 }
