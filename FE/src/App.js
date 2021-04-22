8fa02ee7c5afbf607934108d54af6c0a07233f4f
reminderclock <reminderclock@naver.com>
2021-04-22 11:02:28 +0900

    feat: tab ui

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Main from "./Components/Main/Main";
 import Header from "./Components/Header/Header";
+import Best from './Components/Best/Best';
 const GlobalStyle = styled.div`
 padding : 50px 80px;
 `
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <GlobalStyle>
       <Header />
+      <Best />
       <Main />
     </GlobalStyle>
   );
--- FE/src/Components/Best/Best.js
+++ FE/src/Components/Best/Best.js
@@ -1,7 +1,89 @@
-import React from "react";
+import React,{useState} from "react";
+import styled from 'styled-components';
+import './Best.css';
+// import axios from "axios";
 
+const BestTitle = styled.h2`
+  margin-top: 30px;
+`
 const Best = () => {
-  return <div></div>;
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <>
+    <BestTitle>후기가 증명한 베스트 반찬</BestTitle>
+    <div className="container">
+      <div className="bloc-tabs">
+        <button
+          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(1)}
+        >
+          Tab 1
+        </button>
+        <button
+          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(2)}
+        >
+          Tab 2
+        </button>
+
+        <button
+          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(3)}
+        >
+          Tab 3
+        </button>
+        <button
+          className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(4)}
+        >
+          Tab 4
+        </button>
+        <button
+          className={toggleState === 5 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(5)}
+        >
+          Tab 5
+        </button>
+      </div>
+
+      <div className="content-tabs">
+        <div
+          className={toggleState === 1 ? "content  active-content" : "content"}
+        >
+          <h2>Content 1</h2>
+        </div>
+
+        <div
+          className={toggleState === 2 ? "content  active-content" : "content"}
+        >
+          <h2>Content 2</h2>
+        </div>
+
+        <div
+          className={toggleState === 3 ? "content  active-content" : "content"}
+        >
+          <h2>Content 3</h2>
+        </div>
+        <div
+          className={toggleState === 4 ? "content  active-content" : "content"}
+        >
+          <h2>Content 4</h2>
+        </div>
+        <div
+          className={toggleState === 5 ? "content  active-content" : "content"}
+        >
+          <h2>Content 5</h2>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+  
 };
 
 export default Best;
