b96911f40dd3bc06f03e0d7e4751bda222a9b0f0
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-04-22 21:18:30 +0900

    feat: update controller to return DTO

--- BE/src/main/java/sidedish/controller/CategoryController.java
+++ BE/src/main/java/sidedish/controller/CategoryController.java
@@ -1,16 +1,22 @@
 package sidedish.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import sidedish.domain.Category;
-import sidedish.domain.CategoryType;
 import sidedish.repository.CategoryRepository;
+import sidedish.service.dto.CategoryDTO;
+
 
 @RestController
 @RequestMapping("/banchan-code")
 public class CategoryController {
+
+    Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
     private final CategoryRepository categoryRepository;
 
@@ -18,10 +24,11 @@ public class CategoryController {
         this.categoryRepository = categoryRepository;
     }
 
-    @GetMapping("/main")
-    public ResponseEntity<Category> mainPage() {
-        Category category = categoryRepository.findCategoryByCategoryType("main");
-        return ResponseEntity.ok(category);
+    @GetMapping("/{title}")
+    public ResponseEntity<CategoryDTO> mainPage(@PathVariable String title) {
+        Category category = categoryRepository.findCategoryByTitle(title).orElseThrow(CategoryNotFoundException::new);
+        CategoryDTO categoryDTO = new CategoryDTO(category);
+        return ResponseEntity.ok(categoryDTO);
     }
 
 }
