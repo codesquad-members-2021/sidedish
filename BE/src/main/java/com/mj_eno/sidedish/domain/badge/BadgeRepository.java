c28b1e39db3f5e572652d34083733ee423e7392c
Jeong InHo <63284310+eNoLJ@users.noreply.github.com>
2021-04-22 10:43:51 +0900

    Delete BE directory

--- BE/src/main/java/com/mj_eno/sidedish/domain/badge/BadgeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mj_eno.sidedish.domain.badge;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BadgeRepository extends CrudRepository {
-}
--- BE/src/main/java/com/mj_eno/sidedish/domain/dish/DishRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mj_eno.sidedish.domain.dish;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DishRepository extends CrudRepository {
-}
