224772da92c90318de6fa60fc0e6fd1d0b8ae62d
1song2 <1song2918@gmail.com>
2021-04-22 18:10:09 +0900

    feat : add NetworkManager to perform request

---
+++ iOS/BanchanCode/BanchanCode/Network/NetworkManager.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkManager.swift
+//  BanchanCode
+//
+//  Created by Song on 2021/04/22.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    static func performRequest(urlString: String, completionHandler: @escaping (DishesResponseDTO) -> ()) {
+        AF.request(urlString, method: .get)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: DishesResponseDTO.self) { (response) in
+                switch response.result {
+                case .success(let dishes):
+                    completionHandler(dishes)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+    }
+}
