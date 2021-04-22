5ff25850a31fcac24568c8e2a36cec1051f73fd9
okstring <milkoi7801@gmail.com>
2021-04-22 11:17:07 +0900

    필요없는 파일 및 주석 제거

--- Sidedish/Sidedish/Service/Networking.swift
+++ Sidedish/Sidedish/Service/Networking.swift
@@ -10,16 +10,6 @@ import Alamofire
 
 
 class Networking {
-//    func requestMainCagegiry(url: String, completion: @escaping (Result<Any, AFError>) -> ()) {
-//        AF.request(url, method: .get,
-//                   encoding: JSONEncoding.default,
-//                   headers:  ["Content-Type":"application/json;charset=utf-8"] )
-//            .validate()
-//            .responseJSON(completionHandler: { (response) in
-//                completion(response.result)
-//            })
-//    }
-    
     func requestCategory<T: Decodable>(url: String, completion: @escaping (Result<T, AFError>) -> ()) {
         AF.request(url).validate().responseDecodable(of: T.self) { (response) in
             switch response.result {
