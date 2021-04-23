da543ae3e6c29e5add673998b5b8fd3225e59bb3
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-04-23 18:54:42 +1200

    feat : 베스트반찬들 API 구현 Http상태와 카테고리에 따른 리스트들을 응답

---
+++ backend/src/main/java/develop/baminchan/controller/Message.java
@@ -0,0 +1,24 @@
+package develop.baminchan.controller;
+
+public class Message {
+    public static final int OK = 200;
+    public static final int BAD_REQUEST = 400;
+    public static final int NOT_FOUND = 404;
+    public static final int INTERNAL_SERVER_ERROR = 500;
+
+    private int status;
+    private Object data;
+
+    public Message(int statusEnum, Object data) {
+        this.status = statusEnum;
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
