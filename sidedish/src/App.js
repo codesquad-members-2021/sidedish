5603c3519eb30ce32fb56ed415ee740ca73f6b30
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-04-23 16:48:51 +0900

    [Refactor]: 글로벌 스타일 폰트, 중복부분 리팩토링

--- sidedish/src/App.js
+++ sidedish/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Header from 'component/Header/Header';
 import Main from 'component/Main/Main';
 import './App.css';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import theme from 'theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Header />
       <Main />
     </ThemeProvider>
@@ -14,3 +15,10 @@ function App() {
 }
 
 export default App;
+
+const GlobalStyles = createGlobalStyle`
+body {
+  @import url('https://fonts.googleapis.com/earlyaccess/notosanskr.css');
+  font-family: "Noto Sans KR", normal !important;
+  }
+`;
