a71e59c16b5665d823030f2bd2f73032481bcac8
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-04-23 11:21:06 +0900

    feat: add logger in CategoryController

--- BE/src/main/java/sidedish/controller/CategoryController.java
+++ BE/src/main/java/sidedish/controller/CategoryController.java
@@ -1,22 +1,18 @@
 package sidedish.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 import sidedish.domain.Category;
+import sidedish.exception.CategoryNotFoundException;
 import sidedish.repository.CategoryRepository;
 import sidedish.service.dto.CategoryDTO;
-
 
 @RestController
 @RequestMapping("/banchan-code")
 public class CategoryController {
-
-    Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
     private final CategoryRepository categoryRepository;
 
