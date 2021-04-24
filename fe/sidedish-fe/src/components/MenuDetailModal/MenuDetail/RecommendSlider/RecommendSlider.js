c5cf716456bbb3ac1205f2778e277a1bf9508bc0
cchoongh <cndgh49@naver.com>
2021-04-24 23:33:28 +0900

    [ADD]: Slider 컨트롤 버튼 disabled기능 구현
    * Slider쪽 코드도 같이 고침..
      * onSlide콜백을 받도록 수정

--- fe/sidedish-fe/src/components/MenuDetailModal/MenuDetail/RecommendSlider/RecommendSlider.js
+++ fe/sidedish-fe/src/components/MenuDetailModal/MenuDetail/RecommendSlider/RecommendSlider.js
@@ -26,6 +26,13 @@ const TestItem = styled.div`
 function RecommendSlider({ data = [0, 1, 2, 3, 4, 5, 6] }) { // FIXME
   // const [data, setData] = setState()
   const sliderRef = useRef();
+  const [disableLeftBtn, setDisableLeftBtn] = useState();
+  const [disableRightBtn, setDisableRightBtn] = useState();
+
+  const handleSlide = () => {
+    setDisableLeftBtn(!sliderRef.current.slidableToLeft());
+    setDisableRightBtn(!sliderRef.current.slidableToRight());
+  }
 
   const handleClickLeftBtn = () => {
     sliderRef.current.slideToLeft();
@@ -42,10 +49,17 @@ function RecommendSlider({ data = [0, 1, 2, 3, 4, 5, 6] }) { // FIXME
   return (
     <StyledRecommendSlider>
       <div className="btn-cont">
-        <SlideBtn onClick={handleClickLeftBtn}></SlideBtn>
-        <SlideBtn onClick={handleClickRightBtn}></SlideBtn>
+        <SlideBtn disabled={disableLeftBtn} onClick={handleClickLeftBtn}>
+        </SlideBtn>
+        <SlideBtn disabled={disableRightBtn} onClick={handleClickRightBtn}>
+        </SlideBtn>
       </div>
-      <Slider ref={sliderRef} itemCntOnView={5} items={renderItems()}/>
+      <Slider
+        ref={sliderRef}
+        itemCntOnView={5}
+        items={renderItems()}
+        onSlide={handleSlide}
+        defaultBtn={false}/>
     </StyledRecommendSlider>
   )
 };
