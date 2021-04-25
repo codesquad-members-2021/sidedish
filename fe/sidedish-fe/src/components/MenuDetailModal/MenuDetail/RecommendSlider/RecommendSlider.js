66beb47d6eb215be28c680420fba66837f3baaa6
cchoongh <cndgh49@naver.com>
2021-04-26 00:06:58 +0900

    [ADD]: 아이템 상세 모달 완성도 높이기
    * 추천 슬라이더 스타일 구현
    * 추천 슬라이더 페이지 표시 구현
    * 추천 슬라이더 버튼들 구현
    * 전체적인 margin, padding 조정

--- fe/sidedish-fe/src/components/MenuDetailModal/MenuDetail/RecommendSlider/RecommendSlider.js
+++ fe/sidedish-fe/src/components/MenuDetailModal/MenuDetail/RecommendSlider/RecommendSlider.js
@@ -3,17 +3,49 @@ import styled from 'styled-components';
 import Slider from '../../../common/Slider.js';
 // import MenuCard from '../../../MenuCard/MenuCard.js';
 
+import leftBtnSvg from '../../../../rsc/svg/leftbtn.svg';
+import rightBtnSvg from '../../../../rsc/svg/rightbtn.svg';
+import leftBtnDisabledSvg from '../../../../rsc/svg/leftbtn_disabled.svg';
+import rightBtnDisabledSvg from '../../../../rsc/svg/rightbtn_disabled.svg';
+
 const StyledRecommendSlider = styled.div`
-  background-color: green; // FIXME
   width: 100%;
   height: 100%;
   box-sizing: border-box;
-  /* padding: 20px; */
+  padding: 20px;
+  background: #F5F5F7;
+
+  .title {
+    float: left;
+    font-weight: 800;
+    font-size: 18px;
+    line-height: 26px;
+    color: #333333;
+  }
+
+  .btn-cont {
+    float: right;
+    margin-bottom: 20px;
+    font-size: 14px;
+    line-height: 20px;
+    color: #333333;
+    vertical-align: middle;
+  }
 `;
 
 const SlideBtn = styled.button`
-  width: 30px; // FIXME
-  height: 30px; // FIXME
+  padding: 0 6px;
+  border: none;
+  background-color: transparent;
+
+  &:not(:disabled):hover {
+    opacity: 0.7;
+  }
+
+  .img {
+    pointer-events: none;
+    z-index: -1;
+  }
 `;
 
 const TestItem = styled.div`
@@ -24,7 +56,6 @@ const TestItem = styled.div`
 `;
 
 function RecommendSlider({ data = [0, 1, 2, 3, 4, 5, 6] }) { // FIXME
-  // const [data, setData] = setState()
   const sliderRef = useRef();
   const [disableLeftBtn, setDisableLeftBtn] = useState();
   const [disableRightBtn, setDisableRightBtn] = useState();
@@ -48,10 +79,14 @@ function RecommendSlider({ data = [0, 1, 2, 3, 4, 5, 6] }) { // FIXME
   
   return (
     <StyledRecommendSlider>
+      <div className="title">함께하면 더욱 맛있는 상품</div>
       <div className="btn-cont">
         <SlideBtn disabled={disableLeftBtn} onClick={handleClickLeftBtn}>
+          <img src={disableLeftBtn ? leftBtnDisabledSvg : leftBtnSvg} alt="left button"/>
         </SlideBtn>
+        {sliderRef.current?.getCurrPage()}/{sliderRef.current?.getTotalPage()}
         <SlideBtn disabled={disableRightBtn} onClick={handleClickRightBtn}>
+          <img src={disableRightBtn ? rightBtnDisabledSvg : rightBtnSvg} alt="right button"/>
         </SlideBtn>
       </div>
       <Slider
