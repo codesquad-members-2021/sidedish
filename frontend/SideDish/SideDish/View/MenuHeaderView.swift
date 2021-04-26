5b4118df6c0ba324f87adc86a438125cd715e78b
Sonjh1306 <yale1306@gmail.com>
2021-04-27 00:42:56 +0900

    feat: Create Toaster

--- frontend/SideDish/SideDish/View/MenuHeaderView.swift
+++ frontend/SideDish/SideDish/View/MenuHeaderView.swift
@@ -3,13 +3,12 @@ import UIKit
 
 class MenuHeaderView: UICollectionReusableView {
 
-    @IBOutlet private weak var headerButton: UIButton!
     
-    func configureButtonName(text: String) {
-        self.headerButton.setTitle(text, for: .normal)
+    @IBOutlet weak var headerLabel: UILabel!
+
+    func configureLabelName(text: String) {
+        self.headerLabel.text = text
+
     }
-    
-    @IBAction private func buttonPressed(_ sender: UIButton) {
-        // 토스터 설정하기
-    }
+
 }
