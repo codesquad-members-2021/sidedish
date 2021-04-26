917e3c9ca1f20d910badd40a01c1d4bceb9f2a52
jeong-inho <derosatam76@gmail.com>
2021-04-26 14:53:02 +0900

    Refactor: exception 클래스에 ErrorMessage를 멤버변수로 갖도록 수정
    Errormessage 에 더 많은 정보를 담게 되거나 기능이 확장될 때를 대비하여 exception 클래스에 ErrorMessage를 멤버변수로 갖도록 수정

--- BE/src/main/java/com/mj_eno/sidedish/exception/EntityNotFoundException.java
+++ BE/src/main/java/com/mj_eno/sidedish/exception/EntityNotFoundException.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EntityNotFoundException extends RuntimeException {
 
+    private ErrorMessage errorMessage;
+
     public EntityNotFoundException(String errorMessage) {
         super(errorMessage);
     }
 
     public EntityNotFoundException(ErrorMessage errorMessage) {
         super(errorMessage.getErrorMessage());
+        this.errorMessage = errorMessage;
     }
 }
--- BE/src/main/java/com/mj_eno/sidedish/exception/OrderFailedException.java
+++ BE/src/main/java/com/mj_eno/sidedish/exception/OrderFailedException.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class OrderFailedException extends RuntimeException {
 
+    private ErrorMessage errorMessage;
+
     public OrderFailedException(String errorMessage) {
         super(errorMessage);
     }
 
     public OrderFailedException(ErrorMessage errorMessage) {
         super(errorMessage.getErrorMessage());
+        this.errorMessage = errorMessage;
     }
 }
