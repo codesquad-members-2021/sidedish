63a29bf829cf7e75213f9db90c3f4165bbb6d4f7
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-04-26 22:19:00 +0900

    [FIX] : Json 파일 경로와 ObjectMapper 대한 리팩토링 진행.
    Json 파일 경로 중, 중복되는 두 종류의 글자를 상수로 분리하였고, ItemDTO를 담는 List 또한 중복을 제거하기 위하여 메서드를 생성하였다.
    ObjectMapper는 매번 생성하지 않도록, 따로 분리하여 객체를 생성해주었다.

--- be/sidedish-be/src/main/java/com/codesquad/sidedish/utils/SampleDataFactory.java
+++ be/sidedish-be/src/main/java/com/codesquad/sidedish/utils/SampleDataFactory.java
@@ -1,5 +1,7 @@
 package com.codesquad.sidedish.utils;
 
+import com.codesquad.sidedish.NotReadJsonFileException;
+import com.codesquad.sidedish.NotParsingJsonFileException;
 import com.codesquad.sidedish.web.sidedish.DTO.DetailDTO;
 import com.codesquad.sidedish.web.sidedish.DTO.ItemDTO;
 import com.codesquad.sidedish.web.sidedish.DTO.SidedishDTO;
@@ -17,46 +19,49 @@ import java.util.stream.Collectors;
 
 public class SampleDataFactory {
 
-    private SampleDataFactory() {
+    private static final String JSON_PATH_PREFIX = "sample-data/";
+    private static final String JSON_EXTENSION = ".json";
 
+    public static ObjectMapper objectMapper = new ObjectMapper()
+            .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+    private SampleDataFactory() {
     }
 
     public static List<SidedishDTO> createBestSidedishes() {
-        return jsonToObject("sample-data/best-sidedishes.json", new TypeReference<List<SidedishDTO>>() {
+        return jsonToObject(JSON_PATH_PREFIX + "best-sidedishes" + JSON_EXTENSION, new TypeReference<List<SidedishDTO>>() {
         });
     }
 
     public static List<ItemDTO> createMainSidedishes() {
-        return jsonToObject("sample-data/main-sidedishes.json", new TypeReference<List<ItemDTO>>() {
-        });
+        return createItemDtoListBy("main-sidedishes");
     }
 
     public static List<ItemDTO> createCourseSidedishes() {
-        return jsonToObject("sample-data/course-sidedishes.json", new TypeReference<List<ItemDTO>>() {
-        });
+        return createItemDtoListBy("course-sidedishes");
     }
 
     public static List<ItemDTO> createSoupSidedishes() {
-        return jsonToObject("sample-data/soup-sidedishes.json", new TypeReference<List<ItemDTO>>() {
-        });
+        return createItemDtoListBy("soup-sidedishes");
     }
 
     public static List<ItemDTO> createSideSidedishes() {
-        return jsonToObject("sample-data/side-sidedishes.json", new TypeReference<List<ItemDTO>>() {
-        });
+        return createItemDtoListBy("side-sidedishes");
     }
 
     public static Map<String, DetailDTO> createDetails() {
-        List<DetailDTO> detailDTOs = jsonToObject("sample-data/details.json", new TypeReference<List<DetailDTO>>() {
+        List<DetailDTO> detailDTOs = jsonToObject(JSON_PATH_PREFIX + "details" + JSON_EXTENSION, new TypeReference<List<DetailDTO>>() {
         });
 
         return detailDTOs.stream().collect(Collectors.toMap(DetailDTO::getHash, detailDTO -> detailDTO));
     }
 
-    private static <E> E jsonToObject(String jsonFilePath, TypeReference<E> typeReference) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    private static List<ItemDTO> createItemDtoListBy(String sidedishName) {
+        return jsonToObject(JSON_PATH_PREFIX + sidedishName + JSON_EXTENSION, new TypeReference<List<ItemDTO>>() {
+        });
+    }
 
+    private static <E> E jsonToObject(String jsonFilePath, TypeReference<E> typeReference) {
         try (InputStream is = new ClassPathResource(jsonFilePath).getInputStream()) {
             return objectMapper.readValue(is, typeReference);
         } catch (JsonParseException e) {
