18f75d17d16efd5af37a4b2db59c9cfc3e55944a
jhpark-steve <park9368@naver.com>
2021-04-26 15:47:54 +0900

    feat: Repository 영역 초기화 (#31)
    CoreData 추가, Repository 클래스 추상화

--- ios/sidedish/sidedish/Data/DishRepository.swift
+++ ios/sidedish/sidedish/Data/DishRepository.swift
@@ -6,7 +6,25 @@
 //
 
 import Foundation
+import CoreData
 
-class DishRepository {
+final class DishRepository {
+    private let coreData = CoreDataStorage.shared
+    private var context : NSManagedObjectContext!
+    private var saveSideDish : SaveSideDish!
     
+    init() {
+        
+    }
+    
+    struct 사이드디쉬디티오 : Decodable{
+        private(set) var id: String
+        private(set) var image : String
+        private(set) var title: String
+        private(set) var subtitle: String
+        private(set) var price: Int
+        private(set) var salePrice: Int
+        private(set) var deliveryTypes: [String]?
+        private(set) var badges: [String]?
+    }
 }
