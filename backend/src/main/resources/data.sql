e9ba94f5bf8f48800debb1679fdede842c8f9ea5
Tree <gmldbs1109@naver.com>
2021-04-26 15:28:20 +0900

    chore : Add bestdishes data to data.sql
    - data.sql에 Best API에 있는 데이터 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,12 +1,42 @@
-INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge) VALUES('H9881', 'https://cdn.bmf.kr/_data/product/H9881/910a01a81c49cb75414edb759237501f.jpg', '[소중한식사] 경상도 한상차림', '경상도 명물 요리 세 가지를 한 상에!', 39000, 31200, NULL, 10, '런칭특가');
-INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge) VALUES('HDF4C', 'imageurl', '[딩고] 시그니처 3종 SET', '오늘 저녁은 이상하게 안주빨을 세워보고 싶다!', 32800, 24000, NULL, 10., '런칭특가');
-INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock) VALUES('H7F20', 'imageurl', '[집밥의완성]초여름보양세트', '무더위에 대비하는 6월 한상차림 초여름 보양세트', 26000, 18200, NULL, 10);
+--할인특가 세트상품
+INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge)
+VALUES('H9881', 'https://cdn.bmf.kr/_data/product/H9881/910a01a81c49cb75414edb759237501f.jpg', '[소중한식사] 경상도 한상차림', '경상도 명물 요리 세 가지를 한 상에!', 39000, 31200, NULL, 10, '이벤트특가');
+INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge)
+VALUES('HDF4C', 'image":"https://cdn.bmf.kr/_data/product/HDF4C/954b78f9111bbef54ede9fdcdf1298d8.jpg', '[딩고] 시그니처 3종 SET', '오늘 저녁은 이상하게 안주빨을 세워보고 싶다!', 32800, 24000, NULL, 10., '이벤트특가');
+INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge)
+VALUES('H7F20', 'https://cdn.bmf.kr/_data/product/H7F20/dc164f934d614469d097ccd0cd57cb5e.jpg', '[집밥의완성]초여름보양세트', '무더위에 대비하는 6월 한상차림 초여름 보양세트', 26000, 18200, NULL, 10, '이벤트특가');
 
+--풍성한 고기반찬
+INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge)
+VALUES('HA567', 'https://cdn.bmf.kr/_data/product/HA567/a5bbaa6bfb727030345f26ca9afe27c5.jpg', '[아내의쉐프] 아쉐찜닭', '아내의쉐프가 자신 있게 만들어서 이름도 ''아쉐찜닭''입니다!', 11900, 10700, NULL, 10, '이벤트특가, 론칭특가');
+INSERT INTO dish(hash, top_image, title, description, s_price, section_name, stock, badge)
+VALUES('H206E', 'https://cdn.bmf.kr/_data/product/H206E/325c7aeb1673ffe58052463d93e407b8.jpg', '[애슐리투고] 로스트 콤보 450g', '립과 치킨이 만나  더욱 맛있게', 10900, NULL, 10., '론칭특가');
+INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge)
+VALUES('H75A2', 'https://cdn.bmf.kr/_data/product/H75A2/f0d1fade9f7d16e7e33211a26c3aa469.jpg', '[한옥집]소갈비김치찜 2인분', '부드러운 소갈비와 함께 푹 쪄낸 김치찜!', 16500, 14850, NULL, 10, '이벤트특가');
 
-INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge) VALUES('HBDEF', 'imageurl', '[미노리키친] 규동 250g', '일본인의 소울푸드! 한국인도 좋아하는 소고기덮밥', 6500, 5200, 'main,course', 10, '런칭특가,이벤트특가');
+--편리한 반찬세트
+INSERT INTO dish(hash, top_image, title, description, s_price, section_name, stock, badge)
+VALUES('E055F', 'https://cdn.bmf.kr/_data/product/201410/13/b8b0a0aba3386f030155b6cb0c1c75fa.jpg', '[동네부엌] 사계절 건강식단 작은상', '마포엄마들이 사랑해온 건강반찬', 24000, NULL, 10, '베스트');
+INSERT INTO dish(hash, top_image, title, description, s_price, section_name, stock, badge)
+VALUES('FDAEB', 'https://cdn.bmf.kr/_data/product/201511/04/35365307dba3c23028a55027b29e88c9.jpg', '[집밥의완성] 한상 가득 세트 (3~4인)', '식단 정기배송으로 반찬고민 끝', 32000, NULL, 10, '베스트');
+INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge)
+VALUES('H762E', 'https://cdn.bmf.kr/_data/product/H762E/03cfe3d522df7451ca09ff1e0eede530.jpg', '[옹가솜씨] 한끼뚝딱세트', '한 세트만 있으면 한 끼 뚝딱', 19500, 17500, NULL, 10, '이벤트특가');
 
-INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge) VALUES('HDF73', 'imageurl', '[빅마마의밥친구] 아삭 고소한 연근고기조림 250g', '편식하는 아이도 좋아하는 건강한 연근조림', 0, 5500, 'main,soup', 10, '이벤트특가');
-INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock) VALUES('HF778', 'imageurl', '[소중한식사] 골뱅이무침 195g', '매콤새콤달콤, 반찬으로도 안주로도 좋은', 7000, 6300, 'main', 10);
+--간편한 덮밥요리
+INSERT INTO dish(hash, top_image, title, description, s_price, section_name, stock)
+VALUES('H05FB', 'https://cdn.bmf.kr/_data/product/H05FB/6ff744c6f36ba437a4f8b9e9a000b0b1.jpg', '[옹가솜씨] 소고기쩜짜장 280g', '짜장의 착한 변신', 7200, NULL, 10);
+INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge)
+VALUES('H0699', 'https://cdn.bmf.kr/_data/product/H0699/295db179a21edac9aad382686cb80be8.jpg', '[무명식당]뿌리채소비빔밥 2인분', '각종 뿌리채소를 듬뿍 넣어 지은 뿌리채소영양밥', 5000, 4750, NULL, 10, '이벤트특가');
+INSERT INTO dish(hash, top_image, title, description, n_price, s_price, section_name, stock, badge)
+VALUES('HBDEF', 'https://cdn.bmf.kr/_data/product/HBDEF/289b0fb6c14d2c8cd161205f8dadfa7a.jpg', '[미노리키친] 규동 250g', '일본인의 소울푸드! 한국인도 좋아하는 소고기덮밥', 6500, 5200, NULL, 10, '이벤트특가');
+
+--우리아이 영양반찬
+INSERT INTO dish(hash, top_image, title, description, s_price, section_name, stock)
+VALUES('H82A2', 'https://cdn.bmf.kr/_data/product/H82A2/a43eaf448c21906cd1300735fa9dd7a1.jpg', '[빅마마의밥친구] 입맛감각 살려주는 유부토마토조림 200g', '아이반찬과 다이어트 반찬으로 일석이조!', 4900, NULL, 10);
+INSERT INTO dish(hash, top_image, title, description, s_price, section_name, stock)
+VALUES('H213E', 'https://cdn.bmf.kr/_data/product/H213E/2895aa13fce2c5721f210080a2429562.jpg', '[동네부엌] 수제미트볼조림 300g', '재료부터 다른 수제미트볼조림', 8700, NULL, 10);
+INSERT INTO dish(hash, top_image, title, description, s_price, section_name, stock)
+VALUES('H7A16', 'https://cdn.bmf.kr/_data/product/H7A16/1a9e0f1c8de0e0d268bace68d50b0bf1.jpg', '[Young313] 흰살생선 된장소스구이 200g', '부드러운 명태살을 이용한 특별한 생선요리', 9000, NULL, 10);
 
 
 INSERT INTO image(id, image_url, dish_hash, is_thumb) VALUES (1, 'https://cdn.bmf.kr/_data/product/HDF4C/a5f8637df5a375e3105c12b6efd47538.jpg','H9881', 1);
@@ -23,9 +53,28 @@ INSERT INTO delivery(id, is_monday, dish_hash) VALUES (6, 1, 'H7F20');
 
 
 INSERT INTO best(category_id, name) VALUES (17011200, '할인특가 세트상품');
-INSERT INTO best(category_id, name) VALUES (17011000, '풍성한 고기반');
+INSERT INTO best(category_id, name) VALUES (17011000, '풍성한 고기반찬');
+INSERT INTO best(category_id, name) VALUES (17010200, '편리한 반찬세트');
+INSERT INTO best(category_id, name) VALUES (17010300, '간편한 덮밥요리');
+INSERT INTO best(category_id, name) VALUES (17011400, '우리아이 영양반찬');
 
+--할인특가 세트상품
 INSERT INTO dish_best(dish_hash, category_id) VALUES ('H9881', 17011200);
 INSERT INTO dish_best(dish_hash, category_id) VALUES ('HDF4C', 17011200);
-INSERT INTO dish_best(dish_hash, category_id) VALUES ('HDF73', 17011000);
-INSERT INTO dish_best(dish_hash, category_id) VALUES ('HF778', 17011000);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H7F20', 17011200);
+--풍성한 고기반찬
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('HA567', 17011000);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H206E', 17011000);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H75A2', 17011000);
+--편리한 반찬세트
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('E055F', 17010200);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('FDAEB', 17010200);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H762E', 17010200);
+--간편한 덮밥요리
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H05FB', 17010300);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H0699', 17010300);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('HBDEF', 17010300);
+--우리아이 영양반찬
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H82A2', 17010400);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H213E', 17010400);
+INSERT INTO dish_best(dish_hash, category_id) VALUES ('H7A16', 17010400);
