fdc35b0d21fe45a192413ad3c8bed21b60d998a7
SEONKYU\difke <somedaycode@gmail.com>
2021-04-27 04:07:02 +0900

    Add. 로그인 기능 구현을 위해 cors 추가 및 통신 테스트

--- raqqoon-sidedish/server/server.js
+++ raqqoon-sidedish/server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/github/auth', function (req, res) {
+  res.send({ greet: 'hello' });
+});
 
 app.listen(port, () => {
   console.log(`express is running on ${port}`);
