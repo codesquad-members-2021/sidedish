82dec711a16503ac9f5f5f9a8611db782ae1ca2d
1song2 <1song2918@gmail.com>
2021-04-26 16:46:16 +0900

    chore : Create groups to organize files in

--- iOS/BanchanCode/BanchanCode/Domain/Entities/Dish.swift
+++ iOS/BanchanCode/BanchanCode/Domain/Entities/Dish.swift
@@ -19,23 +19,3 @@ struct Dish {
 struct DishList {
     var dishes: [Dish]
 }
-
-protocol Categorizable {
-    var name: String { get }
-    var sectionTitle: String { get }
-}
-
-struct MainCategory: Categorizable {
-    var name: String = "main"
-    var sectionTitle: String = "모두가 좋아하는 든든한 메인요리"
-}
-
-struct SoupCategory: Categorizable {
-    var name: String = "soup"
-    var sectionTitle: String = "정성이 담긴 뜨끈뜨끈 국물요리"
-}
-
-struct SideCategory: Categorizable {
-    var name: String = "side"
-    var sectionTitle: String = "식탁을 풍성하게 하는 정갈한 밑반찬"
-}
