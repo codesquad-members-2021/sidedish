04610f6bb9a124f29bfd702ce9d01880b274f96f
1song2 <1song2918@gmail.com>
2021-04-26 16:30:35 +0900

    feat : add UseCase

--- iOS/BanchanCode/BanchanCode/Domain/FetchDishesUseCase.swift
+++ iOS/BanchanCode/BanchanCode/Domain/FetchDishesUseCase.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol FetchDishesUseCase {
+    func execute(requestValue: FetchDishesUseCaseRequestValue,
+                 completion: @escaping (Result<DishList, Error>) -> Void)
+}
+
+final class DefaultFetchDishesUseCase: FetchDishesUseCase {
+    func execute(requestValue: FetchDishesUseCaseRequestValue,
+                 completion: @escaping (Result<DishList, Error>) -> Void) {
+        return fetchDishes(category: requestValue.category, completion: { result in
+            completion(result)
+        })
+    }
+    
+    func fetchDishes(category: Categorizable,
+                     completion: @escaping (Result<DishList, Error>) -> Void) {
+        let url = "https://79129275-12cd-405a-80a6-677b968b1977.mock.pstmn.io/banchan-code/\(category.name)"
+        NetworkManager.performRequest(urlString: url) { (responseDTO) in
+            completion(.success(responseDTO.toDomain()))
+        }
+    }
+}
+
+struct FetchDishesUseCaseRequestValue {
+    let category: Categorizable
+}
