8278e8c454db4fae5c0177b9285069d2e6ecf134
gisungchoi <chlrltjd159@naver.com>
2021-04-26 17:18:25 +0900

    refactor : ExceptionController를 글로벌로 배치
    - 어그리게이트 별로 존재 한다고 생각했지만 다른 어그리게이트의 예외도 잡고 있었다
    - 그래서 각자 만들려고 했지만 글로벌로 변경했다.

--- be/sidedish/src/main/java/com/codesquad/sidedish/global/controller/GlobalExceptionController.java
+++ be/sidedish/src/main/java/com/codesquad/sidedish/global/controller/GlobalExceptionController.java
@@ -1,4 +1,4 @@
-package com.codesquad.sidedish.category.controller;
+package com.codesquad.sidedish.global.controller;
 
 import com.codesquad.sidedish.category.exception.CategoryNotFoundException;
 import com.codesquad.sidedish.category.exception.ItemNotFoundException;
@@ -6,14 +6,14 @@ import com.codesquad.sidedish.category.exception.OutOfStockException;
 import com.codesquad.sidedish.event.exception.EventNotFoundException;
 import com.codesquad.sidedish.global.domain.dto.ErrorResponseDTO;
 import com.codesquad.sidedish.global.exception.NotFoundException;
+import com.codesquad.sidedish.image.exception.ImageNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-public class SidedishCategoryExceptionController {
-
+@RestControllerAdvice
+public class GlobalExceptionController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({CategoryNotFoundException.class, ItemNotFoundException.class, EventNotFoundException.class})
     public ErrorResponseDTO handleNotFoundException(NotFoundException exception) {
@@ -23,6 +23,12 @@ public class SidedishCategoryExceptionController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(OutOfStockException.class)
     public ErrorResponseDTO handleOutOfStockException(OutOfStockException exception) {
+        return new ErrorResponseDTO(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ImageNotFoundException.class)
+    public ErrorResponseDTO handleImageNotFoundException(ImageNotFoundException exception) {
         return new ErrorResponseDTO(exception.getMessage());
     }
 }
