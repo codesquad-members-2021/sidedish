1c20720ba9c8d83edb620cb5d94f69b028ed7e9a
Sally Oh <min27604@hotmail.com>
2021-04-26 17:03:30 +0900

    feat: DetailedItemDto 내용수정 및 게터 추가

--- BE/side-dish-backend/src/main/java/com/codesquad/team14/dto/DetailedItemDto.java
+++ BE/side-dish-backend/src/main/java/com/codesquad/team14/dto/DetailedItemDto.java
@@ -1,5 +1,7 @@
 package com.codesquad.team14.dto;
 
+import com.codesquad.team14.domain.Item;
+
 import java.util.List;
 
 public class DetailedItemDto {
@@ -9,37 +11,36 @@ public class DetailedItemDto {
     private final String description;
     private final String deliveryInfo;
     private final String deliveryFee;
-    private final int nPrice;
-    private final int sPrice;
+    private final int normalPrice;
+    private final int salePrice;
     private final int point;
 
     public DetailedItemDto(Long id, String topImage, List<String> thumbImages, String description, String deliveryInfo,
-                           String deliveryFee, int nPrice, int sPrice, int point) {
+                           String deliveryFee, int normalPrice, int salePrice, int point) {
         this.id = id;
         this.topImage = topImage;
         this.thumbImages = thumbImages;
         this.description = description;
         this.deliveryInfo = deliveryInfo;
         this.deliveryFee = deliveryFee;
-        this.nPrice = nPrice;
-        this.sPrice = sPrice;
+        this.normalPrice = normalPrice;
+        this.salePrice = salePrice;
         this.point = point;
     }
 
-    /*
     public static DetailedItemDto from(Item item) {
         return new DetailedItemDto(
                 item.getId(),
-                item.getImages().split(", ")[0],
-                Arrays.asList(item.getImages().split(", ")),
+                item.getTopImage(),
+                item.getImages(),
                 item.getDescription(),
-                item.getDeliveryInfo(),
-                item.getDeliveryFeePolicy(),
-                item.getNPrice(),
-                item.getSPrice(),
-                item.getSPrice() / 100
+                item.getDeliveryTypes(),
+                Item.getDeliveryFeePolicy(),
+                item.getNormalPrice(),
+                item.getSalePrice(),
+                item.getSalePrice() / 100
         );
-    }*/
+    }
 
     public Long getId() {
         return id;
@@ -65,12 +66,12 @@ public class DetailedItemDto {
         return deliveryFee;
     }
 
-    public int getnPrice() {
-        return nPrice;
+    public int getNormalPrice() {
+        return normalPrice;
     }
 
-    public int getsPrice() {
-        return sPrice;
+    public int getSalePrice() {
+        return salePrice;
     }
 
     public int getPoint() {
