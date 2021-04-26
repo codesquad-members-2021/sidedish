01cdef4503c033d0fc8756e89b33c8b17f8e5092
고정완 (Can) <jw.gho@ecubelabs.com>
2021-04-26 16:22:44 +0900

    [test] Softl Assertion 적용

--- backend/src/test/java/com/codesquad/sidedish/SideDish/SideDishApplicationTests.java
+++ backend/src/test/java/com/codesquad/sidedish/SideDish/SideDishApplicationTests.java
@@ -1,53 +1,51 @@
 package com.codesquad.sidedish.SideDish;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.codesquad.sidedish.SideDish.domain.Category;
 import com.codesquad.sidedish.SideDish.domain.CategoryRepository;
-import org.junit.jupiter.api.AfterAll;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
 
 
 @Transactional
 @SpringBootTest
 class SideDishApplicationTests {
 
-	private Logger logger = LoggerFactory.getLogger(SideDishApplicationTests.class);
-	@Autowired
-	private CategoryRepository categoryRepository;
+    private Logger logger = LoggerFactory.getLogger(SideDishApplicationTests.class);
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-	@Test
-	@DisplayName("category 정보 테스트")
-	void readCategory() {
-		Long mainId = 1L;
-		Category main = categoryRepository.findById(mainId).get();
-		logger.info("main {}", main.toString());
+    @Test
+    @DisplayName("category 정보 테스트")
+    void readCategory() {
+        Long mainId = 1L;
+        Category main = categoryRepository.findById(mainId).get();
+        logger.info("main {}", main.toString());
 
-		Long soupId = 2L;
-		Category soup = categoryRepository.findById(soupId).get();
-		logger.info("soup {}", soup.toString());
+        Long soupId = 2L;
+        Category soup = categoryRepository.findById(soupId).get();
+        logger.info("soup {}", soup.toString());
 
-		Long sideId = 3L;
-		Category side = categoryRepository.findById(sideId).get();
-		logger.info("side {}", side.toString());
+        Long sideId = 3L;
+        Category side = categoryRepository.findById(sideId).get();
+        logger.info("side {}", side.toString());
 
-		checkNameAndEndPoint(main, "메인요리", "/main");
-		checkNameAndEndPoint(soup, "국.찌개", "/soup");
-		checkNameAndEndPoint(side, "반찬", "/side");
-	}
+        SoftAssertions softly = new SoftAssertions();
+        checkNameAndEndPoint(softly, main, "메인요리", "/main");
+        checkNameAndEndPoint(softly, soup, "국.찌개", "/soup");
+        checkNameAndEndPoint(softly, side, "반찬", "/side");
+        softly.assertAll();
+    }
 
-	private void checkNameAndEndPoint(Category category, String name, String endPoint) {
-		assertThat(category.getCategoryName()).isEqualTo(name);
-		assertThat(category.getEndPoint()).isEqualTo(endPoint);
-	}
+    private void checkNameAndEndPoint(SoftAssertions softly, Category category, String name, String endPoint) {
+        softly.assertThat(category.getCategoryName()).isEqualTo(name);
+        softly.assertThat(category.getEndPoint()).isEqualTo(endPoint);
+    }
 
 }
