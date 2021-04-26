5059221bbba76a4c5c9944464c8669e62209dfc0
Sonjh1306 <yale1306@gmail.com>
2021-04-26 14:49:30 +0900

    feat: Load Image

--- frontend/SideDish/SideDish/View/MenuCell.swift
+++ frontend/SideDish/SideDish/View/MenuCell.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MenuCell: UICollectionViewCell {
 
+    @IBOutlet weak var cellView: UIView!
     @IBOutlet weak var thumbnailImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
@@ -16,6 +18,8 @@ class MenuCell: UICollectionViewCell {
     @IBOutlet weak var pastPriceLabel: UILabel!
     @IBOutlet weak var eventLabel: UILabel!
     @IBOutlet weak var launchingLabel: UILabel!
+    @IBOutlet weak var contentStackView: UIStackView!
+    @IBOutlet weak var badgeView: UIView!
     
     private let menuCellValidater = MenuCellValidater()
     
@@ -36,6 +40,30 @@ class MenuCell: UICollectionViewCell {
         self.bodyLabel.text = menu.body
         self.currentPriceLabel.text = menu.sPrice
         self.pastPriceLabel.attributedText = menu.nPrice
+        guard let url = URL(string: menu.image) else { return }
+//        guard let data = try? Data(contentsOf: url) else { return }
+//        self.thumbnailImage.image = UIImage(data: data)
+        self.thumbnailImage.kf.setImage(with: url)
+        badge(menu: menu)
+    }
+    
+    func badge(menu: MenuViewModel) {
+        // badge 1
+        if menu.verifyBadges(badges: menu.badges) == [true, false] {
+            self.launchingLabel.isHidden = true
+        // badge 2
+        }else if menu.verifyBadges(badges: menu.badges) == [false, true] {
+            self.eventLabel.isHidden = true
+            self.launchingLabel.leftAnchor.constraint(equalTo: contentStackView.leftAnchor, constant: 0).isActive = true
+        }else if menu.verifyBadges(badges: menu.badges) == [false, false] {
+            self.badgeView.isHidden = true
+            contentStackView.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 16).isActive = true
+            contentStackView.bottomAnchor.constraint(equalTo: cellView.bottomAnchor, constant: -27.5).isActive = true
+        }
+        else{
+            self.eventLabel.isHidden = false
+            self.launchingLabel.isHidden = false
+        }
     }
     
 }
