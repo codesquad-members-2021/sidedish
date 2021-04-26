dd94b696148b8e17d03ca73869ef98b3dd5138fa
Sally Oh <min27604@hotmail.com>
2021-04-26 17:06:24 +0900

    feat: DTO를 이용해 데이터를 내보낼 수 있도록 구현

--- BE/side-dish-backend/src/main/java/com/codesquad/team14/service/ItemService.java
+++ BE/side-dish-backend/src/main/java/com/codesquad/team14/service/ItemService.java
@@ -1,9 +1,12 @@
 package com.codesquad.team14.service;
 
 import com.codesquad.team14.domain.Item;
+import com.codesquad.team14.dto.DetailedItemDto;
+import com.codesquad.team14.dto.ItemDto;
 import com.codesquad.team14.repository.ItemRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,11 +18,19 @@ public class ItemService {
         this.itemRepository = itemRepository;
     }
 
-    public Item readDetailedItem(String category, Long itemId) {
-        return itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당하는 품목이 존재하지 않습니다"));
+    public DetailedItemDto readDetailedItem(String category, Long itemId) {
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당하는 품목이 존재하지 않습니다"));
+        return DetailedItemDto.from(item);
     }
 
-    public List<Item> readAllByCategory(String category) {
-        return itemRepository.findByCategory(category);
+    public List<ItemDto> readAllByCategory(String category) {
+        List<Item> itemList = itemRepository.findByCategory(category);
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (Item item : itemList) {
+            itemDtoList.add(ItemDto.from(item));
+        }
+
+        return itemDtoList;
     }
 }
