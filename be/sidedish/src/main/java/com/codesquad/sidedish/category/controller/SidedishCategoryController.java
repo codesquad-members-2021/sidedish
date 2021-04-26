f74f22ee91008ff671852780fcdf53526120b8ce
gisungchoi <chlrltjd159@naver.com>
2021-04-26 14:20:27 +0900

    refactor : controller에서 동일한 url 매핑

--- be/sidedish/src/main/java/com/codesquad/sidedish/category/controller/SidedishCategoryController.java
+++ be/sidedish/src/main/java/com/codesquad/sidedish/category/controller/SidedishCategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/{category}")
 public class SidedishCategoryController {
 
     private final SidedishItemService itemService;
@@ -17,19 +18,19 @@ public class SidedishCategoryController {
         this.itemService = itemService;
     }
 
-    @GetMapping("/{category}")
+    @GetMapping
     public ResponseEntity<PreviewListDtoWrapper> previewItemList(@PathVariable String category) {
         PreviewListDtoWrapper previewListDtoWrapper = itemService.showPreviewItems(category);
         return new ResponseEntity<>(previewListDtoWrapper, HttpStatus.OK);
     }
 
-    @GetMapping("/{category}/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<DetailItemDtoWrapper> detailItem(@PathVariable String category, @PathVariable Long id) {
         DetailItemDtoWrapper detailDTO = itemService.showDetailItem(category, id);
         return new ResponseEntity<>(detailDTO, HttpStatus.OK);
     }
 
-    @PostMapping("/{category}/{id}")
+    @PostMapping("/{id}")
     public void orderItem(@PathVariable String category, @PathVariable Long id, @RequestBody OrderDTO orderDTO) {
         itemService.order(category, id, orderDTO);
     }
