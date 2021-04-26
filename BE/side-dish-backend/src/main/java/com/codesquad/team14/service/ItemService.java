a0eff7fb0936b6d72e344e5c5c7365293544aa1f
Sally Oh <min27604@hotmail.com>
2021-04-26 16:25:43 +0900

    feat: ItemService 생성
    컨트롤러에서 서비스 사용

--- BE/side-dish-backend/src/main/java/com/codesquad/team14/service/ItemService.java
+++ BE/side-dish-backend/src/main/java/com/codesquad/team14/service/ItemService.java
@@ -1,7 +1,10 @@
 package com.codesquad.team14.service;
 
+import com.codesquad.team14.domain.Item;
 import com.codesquad.team14.repository.ItemRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemService {
@@ -12,5 +15,11 @@ public class ItemService {
         this.itemRepository = itemRepository;
     }
 
+    public Item readDetailedItem(String category, Long itemId) {
+        return itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당하는 품목이 존재하지 않습니다"));
+    }
 
+    public List<Item> readAllByCategory(String category) {
+        return itemRepository.findByCategory(category);
+    }
 }
