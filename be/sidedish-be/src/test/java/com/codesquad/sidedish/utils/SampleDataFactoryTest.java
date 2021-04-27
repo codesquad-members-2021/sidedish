4eea66b33d4bfb0f1a43be7e98f951804eeea5c0
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-04-27 15:10:00 +0900

    [REFACTOR] : resource 구분을 하지 않아 필요없어진 프로필 확인 제거

--- be/sidedish-be/src/test/java/com/codesquad/sidedish/utils/SampleDataFactoryTest.java
+++ be/sidedish-be/src/test/java/com/codesquad/sidedish/utils/SampleDataFactoryTest.java
@@ -14,30 +14,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
-
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 class SampleDataFactoryTest {
-
-    @Autowired
-    Environment environment;
 
     ObjectMapper objectMapper = new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
 
     @Test
     void createBestSidedishes() throws JsonProcessingException {
-        if (!environment.acceptsProfiles(Profiles.of("dev"))) {
-            return;
-        }
         List<SidedishDTO> sidedishDTOs = SampleDataFactory.createBestSidedishes();
 
         assertThat(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(sidedishDTOs))
@@ -47,9 +35,6 @@ class SampleDataFactoryTest {
 
     @Test
     void createMainSidedishes() throws JsonProcessingException {
-        if (!environment.acceptsProfiles(Profiles.of("dev"))) {
-            return;
-        }
         List<ItemDTO> itemDTOS = SampleDataFactory.createMainSidedishes();
 
         assertThat(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(itemDTOS))
@@ -59,9 +44,6 @@ class SampleDataFactoryTest {
 
     @Test
     void createCourseSidedishes() throws JsonProcessingException {
-        if (!environment.acceptsProfiles(Profiles.of("dev"))) {
-            return;
-        }
         List<ItemDTO> itemDTOs = SampleDataFactory.createCourseSidedishes();
 
         assertThat(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(itemDTOs))
@@ -70,9 +52,6 @@ class SampleDataFactoryTest {
 
     @Test
     void createSoupSidedishes() throws JsonProcessingException {
-        if (!environment.acceptsProfiles(Profiles.of("dev"))) {
-            return;
-        }
         List<ItemDTO> itemDTOs = SampleDataFactory.createSoupSidedishes();
 
         assertThat(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(itemDTOs))
@@ -81,9 +60,6 @@ class SampleDataFactoryTest {
 
     @Test
     void createSideSidedishes() throws JsonProcessingException {
-        if (!environment.acceptsProfiles(Profiles.of("dev"))) {
-            return;
-        }
         List<ItemDTO> itemDTOs = SampleDataFactory.createSoupSidedishes();
 
         assertThat(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(itemDTOs))
@@ -94,9 +70,6 @@ class SampleDataFactoryTest {
     @ParameterizedTest
     @MethodSource("createDetailsProvider")
     void createDetails(String hash, String expected) throws JsonProcessingException {
-        if (!environment.acceptsProfiles(Profiles.of("dev"))) {
-            return;
-        }
         Map<String, DetailDTO> createDetails = SampleDataFactory.createDetails();
         assertThat(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(createDetails.get(hash)))
                 .isEqualTo(expected);
