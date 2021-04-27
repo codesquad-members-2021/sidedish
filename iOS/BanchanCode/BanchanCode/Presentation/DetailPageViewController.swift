34528257767aa82be1250dd216a810045e3db5d9
1song2 <1song2918@gmail.com>
2021-04-28 08:40:06 +0900

    feat : add DetailPageViewController

---
+++ iOS/BanchanCode/BanchanCode/Presentation/DetailPageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailPageViewController.swift
+//  BanchanCode
+//
+//  Created by Song on 2021/04/28.
+//
+
+import UIKit
+
+class DetailPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
