8415588b43821523e44fc84bd4a19602e59f4fa0
Hannah Park <ha3158987@gmail.com>
2021-04-27 18:39:56 +0900

    [#19] refactor: :hammer: 리팩토링

--- src/component/Context.jsx
+++ src/component/Context.jsx
@@ -1,4 +1,4 @@
-import { useReducer, createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import API from "../common/api.js";
 
 const DetailDataContext = createContext();
