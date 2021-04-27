b338d206c85f616d55567f9820b90f274ee9bacc
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-04-27 14:44:23 +0900

    Add: OctoKit dependency 추가

--- Side-dish/Side-dish/PresentationLayer/ViewController/AuthViewController.swift
+++ Side-dish/Side-dish/PresentationLayer/ViewController/AuthViewController.swift
@@ -9,35 +9,17 @@ import UIKit
 import SafariServices
 
 class AuthViewController: UIViewController {
-    let oAuthService: OAuthService
-        init(oAuthService: OAuthService) {
-            self.oAuthService = oAuthService
-            super.init(nibName: nil, bundle: nil)
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("sdf")
         navigationController?.navigationBar.isHidden = true
     }
     
     @IBAction func loginButton(_ sender: UIButton) {
-//        guard let tempVC = self.storyboard?.instantiateViewController(withIdentifier: "TempViewController") as? TempViewController else {
-//            print("meh")
-//            return
-//        }
-//        navigationController?.navigationBar.isHidden = false
-//        self.navigationController?.pushViewController(tempVC, animated: true)
-        guard let url = oAuthService?.getAuthPageUrl() else { return }
-
-               let safariVC = SFSafariViewController(url: url)
-               safariVC.modalPresentationStyle = .fullScreen
-               present(safariVC, animated: true, completion: nil)
-        
+        let url: URL = URL(string: "https://www.google.com")!
+        let safariVC = SFSafariViewController(url: url)
+        safariVC.modalPresentationStyle = .fullScreen
+        present(safariVC, animated: true, completion: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
--- Side-dish/Side-dish/SceneDelegate.swift
+++ Side-dish/Side-dish/SceneDelegate.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    var dependencyContainer = AppDependencyContainer()
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -18,23 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return
         }
         
-        rootViewController.oAuthService = OAuthService(oauthClient: LocalOauthClient())
-        
         window = UIWindow(frame: screen.coordinateSpace.bounds)
         window?.windowScene = screen
         window?.rootViewController = UINavigationController(rootViewController: rootViewController)
         window?.makeKeyAndVisible()
     }
-    
-    // app will react to the custom URL schema and we need to handle it properly. To be notified when such interaction happen while the app is in the foreground we need to override this method of the Scene delegate
-    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-            if let urlContext = URLContexts.first {
-                let url = urlContext.url
-                if let deepLink = DeepLink(url: url) {
-                    dependencyContainer.deepLinkHandler.handleDeepLinkIfPossible(deepLink: deepLink)
-                }
-            }
-        }
 }
 
 
