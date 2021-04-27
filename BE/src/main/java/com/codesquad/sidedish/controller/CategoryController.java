bf1f2c7fa390cc1b2673ecd14ea68a3a530e890c
95degree <95degree@naver.com>
2021-04-27 16:16:06 +0900

    refactor : @RequestMapping 변경 #4
    RequestMapping을 /categories/{category:main|soup|side}"로 변경하고 그 에 맞게 메소드를 수정

--- BE/src/main/java/com/codesquad/sidedish/controller/CategoryController.java
+++ BE/src/main/java/com/codesquad/sidedish/controller/CategoryController.java
@@ -1,59 +1,33 @@
 package com.codesquad.sidedish.controller;
 
-import com.codesquad.sidedish.CategoryType;
-import com.codesquad.sidedish.domain.Category;
 import com.codesquad.sidedish.domain.Dish;
-import com.codesquad.sidedish.repository.CategoryRepository;
+import com.codesquad.sidedish.dto.CategoryResponseDto;
+import com.codesquad.sidedish.dto.DishDetailResponseDto;
 import com.codesquad.sidedish.service.CategoryService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/categories/{category:main|soup|side}")
 public class CategoryController {
-
-    private CategoryRepository categoryRepository;
 
     private CategoryService categoryService;
 
-    public CategoryController(CategoryService categoryService, CategoryRepository categoryRepository) {
+    public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
-        this.categoryRepository = categoryRepository;
     }
 
-    @PostMapping("/dishes")
-    public void addDish(@RequestBody Dish dish) {
-        Category category = categoryRepository.findById(dish.getCategoryId()).get();
-        category.addDish(dish);
-        categoryRepository.save(category);
+    @PostMapping
+    public void addDish(@PathVariable String category, @RequestBody Dish dish) {
+        categoryService.addDish(category, dish);
     }
 
-    @GetMapping("/main")
-    public void readMainDishes() {
-        categoryService.readDishesByCategory(CategoryType.MAIN);
+    @GetMapping
+    public CategoryResponseDto readDishes(@PathVariable String category) {
+        return categoryService.readDishesByCategory(category);
     }
 
-    @GetMapping("/soup")
-    public void readSoupDishes() {
-        categoryService.readDishesByCategory(CategoryType.SOUP);
-    }
-
-    @GetMapping("/side")
-    public void readSideDishes() {
-        categoryService.readDishesByCategory(CategoryType.SIDE);
-    }
-
-    @GetMapping("/main/{dishId}")
-    public void readMainDish(@PathVariable String dishId) {
-        categoryService.readDishByCategoryTypeAndDishId(CategoryType.MAIN, dishId);
-    }
-
-    @GetMapping("/soup/{dishId}")
-    public void readSoupDish(@PathVariable String dishId) {
-        categoryService.readDishByCategoryTypeAndDishId(CategoryType.SOUP, dishId);
-    }
-
-    @GetMapping("/side/{dishId}")
-    public void readSideDish(@PathVariable String dishId) {
-        categoryService.readDishByCategoryTypeAndDishId(CategoryType.SIDE, dishId);
+    @GetMapping("/{dishId}")
+    public DishDetailResponseDto readDish(@PathVariable String category, @PathVariable String dishId) {
+        return categoryService.readDishByCategoryAndDishId(category, dishId);
     }
 }
