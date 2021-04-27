4abf57fae5b982b404f9c50d7e2933247fb4acb0
lena <sue822330@gmail.com>
2021-04-27 11:25:15 +0900

    Add Detail Scroll View

--- Sidedish/Sidedish/DetailViewController.swift
+++ Sidedish/Sidedish/DetailViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var detailImages: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // 상세이미지 추가
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1).isActive = true
+        detailImages.addArrangedSubview(imageView)
     }
     
     override func viewWillAppear(_ animated: Bool) {
