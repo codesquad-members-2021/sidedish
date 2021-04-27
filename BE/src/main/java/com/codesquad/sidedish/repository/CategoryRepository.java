69ffe56fd93ced2ff0191842a757df6ea3ca70c4
95degree <95degree@naver.com>
2021-04-27 16:15:07 +0900

    feat : 불필요한 메소드 제거#4
    이전의 불필요한 메소드들을 제거하고 꼭 필요한 findCategoryByType()를 추가

--- BE/src/main/java/com/codesquad/sidedish/repository/CategoryRepository.java
+++ BE/src/main/java/com/codesquad/sidedish/repository/CategoryRepository.java
@@ -1,22 +1,8 @@
 package com.codesquad.sidedish.repository;
 
 import com.codesquad.sidedish.domain.Category;
-import com.codesquad.sidedish.domain.Dish;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-    @Query("SELECT * FROM dish WHERE type = :categoryType")
-    public List<Dish> findDishesByCategory(String categoryType);
-//String id, String mainImage, String alt, String deliveryType, String title, String description,
-//                String normalPrice, String sellingPrice, String badge, Long categoryId, String topImage, String thumbImages,
-//                String deliveryInfo, String deliveryFee, String detailSection, int stock, String point
-
-    @Query("SELECT * FROM dish WHERE category = :categoryTypeNum AND id = :dishId")
-    public Dish findDishByCategoryTypeAndDishId(Long categoryTypeNum, String dishId);
-
-    @Query("select * from category where type = :categoryType")
-    public Category findByType(String categoryType);
+    Category findCategoryByType(String categoryType);
 }
