0075f99f3be009e44f9fe366e3c7eb2ca2697259
jjunyjjuny <jjuny546@gmail.com>
2021-04-28 01:50:27 +0900

    feat : Add carousel customMode

--- src/js/Carousel/Carousel.js
+++ src/js/Carousel/Carousel.js
@@ -3,27 +3,21 @@ import styled, { css } from "styled-components";
 import { HiOutlineChevronLeft, HiOutlineChevronRight } from "react-icons/hi";
 
 const carouselContext = () => {
-  const carouselStates = [];
-  const moveControllers = {};
-
+  const _carouselDatas = {};
+  const _moveControllers = {};
   const transitionDefault = "all .5s";
 
   const Carousel = (props) => {
-    const state = _findCarousel(props.carouselId) ?? props;
-    if (state === props) {
-      carouselStates.push(props);
-    }
-    const {
-      carouselId,
-      children: items,
-      itemsPerPeice,
-      onClickItem,
-      gap = "0.5rem",
-      autoFit = false,
-    } = state;
+    const { carouselId, customMode } = props;
 
-    const list = _createVirtureSlides(items, itemsPerPeice);
-    const [slides, setSlides] = useState(list);
+    const state = _getData(props, carouselId);
+
+    const { children: items, itemsPerPeice = 1, autoFit = false } = state;
+    const { gap = autoFit ? "0.5rem" : "0rem" } = state;
+
+    const slideList = _createSlideList(items, itemsPerPeice);
+
+    const [slides, setSlides] = useState(slideList);
     const [x, setX] = useState(-100);
     const [moving, setMoving] = useState(false);
     const [trasitionValue, setTransitionValue] = useState(transitionDefault);
@@ -33,16 +27,19 @@ const carouselContext = () => {
       setX((prevX) => prevX + direction * 100);
       setMoving(true);
     };
-    setController(carouselId, onMove);
+
+    _setController(carouselId, onMove);
+
     const onTransitionEnd = () => {
       setMoving(false);
-      if (x === -itemsPerPeice * 100) {
+      console.log(x, -(slideList.length - 1) * 100);
+      if (x === -(slideList.length - 1) * 100) {
         setTransitionValue("none");
         setSlides((slides) => {
           const slide = slides.shift();
           return [...slides, slide];
         });
-        setX(-itemsPerPeice * 100 + 100);
+        setX(-(slideList.length - 1) * 100 + 100);
       } else if (x === 0) {
         setTransitionValue("none");
         setSlides((slides) => {
@@ -65,64 +62,104 @@ const carouselContext = () => {
     const renderList = (list) => {
       return list.map((slide, index) => {
         return (
-          <Slide key={index}>
+          <Panel key={index} gap={gap}>
             {slide.map((item, index) => (
-              <SlideItem key={index}>{item}</SlideItem>
+              <Item
+                key={index}
+                autoFit={autoFit}
+                itemsPerPeice={itemsPerPeice}
+                gap={gap}
+              >
+                {item}
+              </Item>
             ))}
-          </Slide>
+          </Panel>
         );
       });
     };
-    
     return (
-      <>
-        <Wrap className="App">
-          <CarouselContainer style={ulStyles} onTransitionEnd={onTransitionEnd}>
+      <Wrapper customMode={customMode}>
+        {!customMode && (
+          <Button prev onClick={() => onMove(+1)}>
+            <HiOutlineChevronLeft />
+          </Button>
+        )}
+        <CarouselContainer>
+          <Slider gap={gap} style={ulStyles} onTransitionEnd={onTransitionEnd}>
             {renderList(slides)}
-          </CarouselContainer>
-        </Wrap>
-      </>
+          </Slider>
+        </CarouselContainer>
+        {!customMode && (
+          <Button next onClick={() => onMove(-1)}>
+            <HiOutlineChevronRight />
+          </Button>
+        )}
+      </Wrapper>
     );
   };
 
-  const PrevButton = ({ carouselId }) => {
+  const Controller = ({
+    children,
+    carouselId,
+    style = {},
+    prev = false,
+    next = false,
+  }) => {
+    if (!prev && !next) {
+      throw new Error(
+        'MissingRequiredPropertyError: you have to pass "prev" or "next" as direction property to the controller.'
+      );
+    }
+    if (!carouselId) {
+      throw new Error(
+        "MissingRequiredPropertyError: you have to pass carouselId to the controller."
+      );
+    }
+    const direction = prev ? 1 : next ? -1 : 0;
+    const button =
+      children ??
+      (prev ? <HiOutlineChevronLeft /> : next ? <HiOutlineChevronRight /> : "");
     return (
-      <Button onClick={() => moveControllers[carouselId](+1)}>
-        <HiOutlineChevronLeft />
+      <Button
+        style={style}
+        onClick={() => _moveControllers[carouselId](direction)}
+      >
+        {button}
       </Button>
     );
   };
 
-  const NextButton = ({ carouselId }) => {
-    return (
-      <Button onClick={() => moveControllers[carouselId](-1)}>
-        <HiOutlineChevronRight />
-      </Button>
-    );
+  const _setController = (id, move) => {
+    _moveControllers[id] = move;
+  };
+  const _getData = (props, targetId) => {
+    const { customMode, children: items, itemsPerPeice, autoFit, gap } = props;
+    const data = { children: items, itemsPerPeice, autoFit, gap };
+
+    if (!customMode) {
+      return data;
+    }
+    if (_isThereCarouselData(targetId)) {
+      return _getCarouselData(targetId);
+    } else {
+      _setData(data, targetId);
+      return data;
+    }
+  };
+  const _setData = (data, id) => {
+    _carouselDatas[id] = data;
+  };
+  const _isThereCarouselData = (targetId) => {
+    return targetId in _carouselDatas;
+  };
+  const _getCarouselData = (targetId) => {
+    return _carouselDatas[targetId];
   };
 
-  //built-in
-
-  const setController = (id, move) => {
-    moveControllers[id] = move;
-  };
-
-  const _isThereState = (targetId) => {
-    return _findCarousel(targetId) ? true : false;
-  };
-
-  const _slideCarousel = (targetId, dir) => {
-    const TargetCarousel = _findCarousel(targetId);
-  };
-
-  const _findCarousel = (targetId) => {
-    return carouselStates.find((state) => state.id === targetId);
-  };
-
-  return { Carousel, PrevButton, NextButton };
+  return { Carousel, Controller };
 };
 
-const _createVirtureSlides = (items, itemsPerPeice) => {
+const _createSlideList = (items, itemsPerPeice) => {
   if (!Array.isArray(items)) {
     return [items];
   }
@@ -132,41 +169,51 @@ const _createVirtureSlides = (items, itemsPerPeice) => {
     result[i] ? (result[i][j] = item) : (result[i] = [item]);
     return result;
   }, []);
+  newItems.unshift(newItems.pop());
   return newItems;
 };
+
 const _divmod = (a, b) => {
   return [parseInt(a / b), a % b];
 };
-const Wrap = styled.div`
-  margin: 50px 0;
-`;
-const CarouselContent = styled.div`
-  width: 90%;
-  overflow: hidden;
+const Wrapper = styled.div`
+  ${({ customMode }) =>
+    !customMode &&
+    css`
+      display: flex;
+      align-items: center;
+    `}
 `;
 const CarouselContainer = styled.div`
-  display: flex;
+  overflow: hidden;
 `;
-const Slide = styled.ul`
-  background: tan;
-  border: 1px solid black;
+const Slider = styled.div`
+  display: flex;
+  width: 100%;
+  padding-right: ${({ gap }) => `${gap}`};
+`;
+const Panel = styled.ul`
+  margin: 0;
   display: flex;
   width: 100%;
   flex: 1 0 auto;
   padding: 0;
-  padding-right: ${({ gap }) => `${gap}`};
-  &::first-child {
-  }
+  margin-right: ${({ gap }) => `${gap}`};
 `;
-const SlideItem = styled.li`
+const Item = styled.li`
   list-style-type: none;
-  ${({ autoFit, itemsPerPeice, gap }) => css`
-    width: ${autoFit ? `calc(100%/${itemsPerPeice})` : "auto"};
-    & + & {
-      margin-left: ${gap};
-    }
-  `}
+  background: tan;
+  ${({ autoFit, itemsPerPeice, gap }) =>
+    autoFit &&
+    css`
+      width: ${autoFit ? `calc(100%/${itemsPerPeice})` : "auto"};
+      & + & {
+        margin-left: ${gap};
+      }
+    `}
 `;
-const Button = styled.div``;
+const Button = styled.div`
+  font-size: 2rem;
+`;
 
 export default carouselContext();
