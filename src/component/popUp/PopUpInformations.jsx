f2f604b95606bf33bdba9e0d4b52bb3d5542fd26
Hannah Park <ha3158987@gmail.com>
2021-04-27 10:51:58 +0900

    [#16] feat: :sparkles: 수량정보 컴포넌트 분리

--- src/component/popUp/PopUpInformations.jsx
+++ src/component/popUp/PopUpInformations.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import Label from '../util/Label';
-import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
+import PopUpItemCountContainer from './PopUpItemCountContainer';
 
 const PopUpInformationsStyle = styled.div`
 `;
@@ -57,44 +57,6 @@ const PopUpItemBuyingInformationTitle = styled.div`
 
 const PopUpItemBuyingInformationContent = styled.div`
     line-height: 1.5rem;
-`;
-const PopUpItemCountContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 1.5rem 0;
-    border-bottom: 1px solid #e0e0e0;
-    & > *:last-child {
-        display: grid;
-        grid-template-columns: 5rem 2rem;
-        line-height: 0;
-    }
-`;
-
-const PopUpItemCountTitle = styled.div`
-    color: #828282;
-    width: 5rem;
-`;
-
-const PopUpItemCount = styled.input.attrs({
-  type: 'text'
-})`
-    font-size: 1rem;
-    padding: 0rem 1rem;
-    border: 1px solid #E0E0E0;
-    text-align: center;
-`;
-
-const PopUpItemCountBtns = styled.div`
-    border: 1px solid #E0E0E0;
-    border-left: 0px;
-`;
-
-const PopUpItemCountBtn = styled.button`
-    width: 100%;
-    &:first-child {
-        border-bottom: 1px solid #E0E0E0;
-    }
 `;
 
 const PopUpItemTotalPriceContainer = styled.div`
@@ -159,16 +121,7 @@ export default function PopUpInformations({ detailData }) {
                     <PopUpItemBuyingInformationContent>{delivery_fee}</PopUpItemBuyingInformationContent>
                 </PopUpItemBuyingInformation>
             </PopUpItemBuyingInformations>
-            <PopUpItemCountContainer>
-                <PopUpItemCountTitle>수량</PopUpItemCountTitle>
-                <div>
-                    <PopUpItemCount></PopUpItemCount>
-                    <PopUpItemCountBtns>
-                        <PopUpItemCountBtn><FaAngleUp/></PopUpItemCountBtn>
-                        <PopUpItemCountBtn><FaAngleDown/></PopUpItemCountBtn>
-                    </PopUpItemCountBtns>
-                </div>
-            </PopUpItemCountContainer>
+            <PopUpItemCountContainer stock={stock}/>
             <PopUpItemTotalPriceContainer>
                 <PopUpItemTotalPriceTitle>총 주문금액</PopUpItemTotalPriceTitle>
                 <PopUpTotalPrice>{price}</PopUpTotalPrice>
