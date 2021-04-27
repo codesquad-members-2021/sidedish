5b06d678cea45b243e213a4cf73250aacaada1c3
Tree <gmldbs1109@naver.com>
2021-04-27 16:30:34 +0900

    #32] Refactor : Change response wrapping class from Map to Custom DTO
    -Map으로 리턴하는 메소드를 BestDishDTO로 리턴하도록 변경

--- backend/src/main/java/com/team15/sidedish/controller/BestController.java
+++ backend/src/main/java/com/team15/sidedish/controller/BestController.java
@@ -1,15 +1,12 @@
 package com.team15.sidedish.controller;
 
 import com.team15.sidedish.dto.BestDTO;
+import com.team15.sidedish.dto.BestDishesDTO;
 import com.team15.sidedish.service.BestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/best")
