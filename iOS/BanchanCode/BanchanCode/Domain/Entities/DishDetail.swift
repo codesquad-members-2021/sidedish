d90a8a7482ff72a5bc11d20de51649af335964ea
1song2 <1song2918@gmail.com>
2021-04-28 11:36:59 +0900

    feat : add DishDetail and DishDetailResponseDTO

---
+++ iOS/BanchanCode/BanchanCode/Domain/Entities/DishDetail.swift
@@ -0,0 +1,21 @@
+//
+//  DishDetail.swift
+//  BanchanCode
+//
+//  Created by Song on 2021/04/28.
+//
+
+import Foundation
+
+struct DishDetail {
+    let id: Int
+    let name: String
+    let description: String
+    let prices: [Int]
+    let badges: [String]
+    let stock: Int
+    let point: Int
+    let deliveryInfo: String
+    let thumbImages: [String]
+    let detailImages: [String]
+}
