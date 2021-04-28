5d5691c74e32d82ee0a361e94da874d054299db7
1song2 <1song2918@gmail.com>
2021-04-28 20:18:52 +0900

    feat : add FetchDishDetailsUseCase

---
+++ iOS/BanchanCode/BanchanCode/Domain/UseCases/FetchDishDetailsUseCase.swift
@@ -0,0 +1,37 @@
+//
+//  FetchDishDetailsUseCase.swift
+//  BanchanCode
+//
+//  Created by Song on 2021/04/28.
+//
+
+import Foundation
+
+final class FetchDishDetailsUseCase: UseCase {
+    let networkManager = NetworkManager()
+    struct RequestValue {
+        let categoryName: String
+        let id: Int
+    }
+    typealias ResultValue = (Result<DishDetail, Error>)
+    
+    private let requestValue: RequestValue
+    private let completion: (ResultValue) -> Void
+    
+    init(requestValue: RequestValue,
+         completion: @escaping (ResultValue) -> Void) {
+        self.requestValue = requestValue
+        self.completion = completion
+    }
+    
+    func start() {
+        fetchDishDetails(categoryName: requestValue.categoryName, id: requestValue.id, completion: completion)
+    }
+    
+    private func fetchDishDetails(categoryName: String, id: Int, completion: @escaping (Result<DishDetail, Error>) -> Void) {
+        let url = "http://ec2-3-36-241-44.ap-northeast-2.compute.amazonaws.com:8080/banchan-code/\(categoryName)/\(id)"
+        networkManager.performRequestDishDetail(urlString: url) { responseDTO in
+            self.completion(.success(responseDTO.toDomain()))
+        }
+    }
+}
---
+++ iOS/BanchanCode/BanchanCode/Domain/UseCases/UseCase.swift
@@ -0,0 +1,14 @@
+//
+//  UseCase.swift
+//  BanchanCode
+//
+//  Created by Song on 2021/04/28.
+//
+
+import Foundation
+
+public protocol UseCase {
+    //@discardableResult
+    //func start() -> Cancellable?
+    func start()
+}
