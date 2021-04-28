e352273aa7159e116394fe37ce38c30db048b18c
95degree <95degree@naver.com>
2021-04-28 17:48:49 +0900

    feat : 주문하기 api 생성 #4
    - Dish에 checkStock(),updateStock() 추가
    - controller에 OrderDish() 추가

--- BE/src/main/java/com/codesquad/sidedish/controller/CategoryController.java
+++ BE/src/main/java/com/codesquad/sidedish/controller/CategoryController.java
@@ -4,9 +4,8 @@ import com.codesquad.sidedish.domain.Category;
 import com.codesquad.sidedish.domain.Dish;
 import com.codesquad.sidedish.dto.CategoryResponseDto;
 import com.codesquad.sidedish.dto.DishDetailResponseDto;
-import com.codesquad.sidedish.dto.DishResponseDto;
+import com.codesquad.sidedish.dto.ResponseDto;
 import com.codesquad.sidedish.service.CategoryService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,8 +20,8 @@ public class CategoryController {
     }
 
     @PostMapping
-    public void addDish(@PathVariable String category, @RequestBody Dish dish) {
-        categoryService.addDish(category, dish);
+    public void addDish(@PathVariable String type, @RequestBody Dish dish) {
+        categoryService.addDish(type, dish);
     }
 
     @GetMapping
@@ -33,7 +32,15 @@ public class CategoryController {
 
     @GetMapping("/{dishId}")
     public DishDetailResponseDto readDish(@PathVariable String type, @PathVariable String dishId) {
-        Dish dish = categoryService.findDishByTypeAndId(type,dishId);
+        Dish dish = categoryService.findDishByTypeAndId(type, dishId);
         return DishDetailResponseDto.of(dish);
+    }
+
+    @PutMapping("/{dishId}")
+    public ResponseEntity<ResponseDto> OrderDish(@PathVariable String type, @PathVariable String dishId, @RequestParam("count") int orderSize) {
+        if (categoryService.orderDish(type, dishId, orderSize)) {
+            return ResponseEntity.ok().body(new ResponseDto("Success"));
+        }
+        return ResponseEntity.badRequest().body(new ResponseDto("반찬 재고 부족"));
     }
 }
