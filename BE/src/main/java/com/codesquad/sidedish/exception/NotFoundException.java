1269657dd7f7a35f3347ded9b4989950d43d8c39
95degree <95degree@naver.com>
2021-04-29 16:52:12 +0900

    feat : 에러 발생시 이유가 message에 담기 도록 변경#30

--- BE/src/main/java/com/codesquad/sidedish/exception/NotFoundException.java
+++ BE/src/main/java/com/codesquad/sidedish/exception/NotFoundException.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
+    private String message;
+
     public NotFoundException(String message) {
+        this.message = message;
     }
 
-    private String errorMessage;
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
