59debf3d05f86a2993473c1ec1fd9361907158ef
jeong-inho <derosatam76@gmail.com>
2021-04-29 18:12:36 +0900

    Feat: UserService 클래스 생성
    UserController 클래스에 있던 비지니스 로직은 UserService 클래스를 만들어 이동, code를 통해 토큰을 얻어오는 tokenRequestApi 메소드, 토큰으로 유저 정보를 얻어오는 userInfoRequestApi 메소드, 토큰으로 유저 이멘일을 얻어오는 emailRequestApi 메소드 생성

---
+++ BE/src/main/java/com/mj_eno/sidedish/service/UserService.java
@@ -0,0 +1,93 @@
+package com.mj_eno.sidedish.service;
+
+import com.mj_eno.sidedish.domain.user.User;
+import com.mj_eno.sidedish.domain.user.UserRepository;
+import com.mj_eno.sidedish.exception.EntityNotFoundException;
+import com.mj_eno.sidedish.exception.ErrorMessage;
+import com.mj_eno.sidedish.web.dto.EmailDTO;
+import com.mj_eno.sidedish.web.dto.TokenDTO;
+import com.mj_eno.sidedish.web.dto.UserInfoDTO;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@PropertySource("classpath:/oauth.properties")
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
+    private final Environment environment;
+
+    public UserService(UserRepository userRepository, Environment environment, RestTemplateBuilder restTemplateBuilder) {
+        this.userRepository = userRepository;
+        this.restTemplate = restTemplateBuilder.build();
+        this.environment = environment;
+    }
+
+    public User login(String code) {
+        TokenDTO tokenDTO = tokenRequestApi(code);
+        UserInfoDTO userInfoDTO = userInfoRequestApi(tokenDTO.getAccess_token());
+        EmailDTO emailDTO = emailRequestApi(tokenDTO.getAccess_token());
+
+        if (verifyUser(userInfoDTO)) {
+            User user = findByUserId(userInfoDTO);
+            user.update(userInfoDTO, emailDTO, tokenDTO);
+            return userRepository.save(user);
+        }
+        User user = new User(userInfoDTO, emailDTO, tokenDTO);
+        return userRepository.save(user);
+    }
+
+    private TokenDTO tokenRequestApi(String code) {
+        String id = environment.getProperty("github.client.id");
+        String secret = environment.getProperty("github.secret");
+        String url = environment.getProperty("github.access.token.url");
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("client_id", id)
+                .queryParam("client_secret", secret)
+                .queryParam("code", code);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
+        return restTemplate.exchange(builder.toUriString(), HttpMethod.POST, httpEntity, TokenDTO.class).getBody();
+    }
+
+    private UserInfoDTO userInfoRequestApi(String token) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.github.com/user");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.AUTHORIZATION, "token " + token);
+        HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
+        return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, httpEntity, UserInfoDTO.class).getBody();
+    }
+
+    private EmailDTO emailRequestApi(String token) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.github.com/user/emails");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.AUTHORIZATION, "token " + token);
+        HttpEntity<?> httpEntity3 = new HttpEntity<>(httpHeaders);
+        List<EmailDTO> emailDTOList = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, httpEntity3, List.class).getBody();
+        // 문제있음
+        return emailDTOList.get(0);
+    }
+
+    private boolean verifyUser(UserInfoDTO userInfoDTO) {
+        return userRepository.findByUserId(userInfoDTO.getLogin()).isPresent();
+    }
+
+    private User findByUserId(UserInfoDTO userInfoDTO) {
+        return userRepository.findByUserId(userInfoDTO.getLogin()).orElseThrow(
+                () -> new EntityNotFoundException(ErrorMessage.ENTITY_NOT_FOUND)
+        );
+    }
+}
