bba64b8f52389ab1f6c1c0c7a2fdd8d0afcb8e6f
rla36 <bhmarsd@naver.com>
2021-04-29 15:19:14 +0900

    feat: add Exception and handler
    add NotFoundException and make DishNotFoundException and
    CategoryNotFoundException extend NotFoundException.
    
    add ExceptionHandler to handle NotFoundException

--- BE/src/main/java/sidedish/exception/CategoryNotFoundException.java
+++ BE/src/main/java/sidedish/exception/CategoryNotFoundException.java
@@ -1,8 +1,9 @@
 package sidedish.exception;
 
-public class CategoryNotFoundException extends RuntimeException{
+public class CategoryNotFoundException extends NotFoundException {
+    private static final String CATEGORY_NOT_FOUND_MESSAGE = "해당 카테고리가 존재하지 않습니다!";
 
     public CategoryNotFoundException() {
-        super("해당 카테고리가 존재하지 않습니다!");
+        super(CATEGORY_NOT_FOUND_MESSAGE);
     }
 }
--- BE/src/main/java/sidedish/exception/DishNotFoundException.java
+++ BE/src/main/java/sidedish/exception/DishNotFoundException.java
@@ -1,8 +1,9 @@
 package sidedish.exception;
 
-public class DishNotFoundException extends RuntimeException{
+public class DishNotFoundException extends NotFoundException {
+    private static final String DISH_NOT_FOUND_MESSAGE = "해당 dish가 존재하지 않습니다!";
 
     public DishNotFoundException() {
-        super("해당 dish가 존재하지 않습니다!");
+        super(DISH_NOT_FOUND_MESSAGE);
     }
 }
