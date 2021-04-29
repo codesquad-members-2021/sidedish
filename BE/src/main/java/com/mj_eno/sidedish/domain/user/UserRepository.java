73b2daf59973c2089d92138cb50f52ca99c0c700
jeong-inho <derosatam76@gmail.com>
2021-04-29 21:58:23 +0900

    Feat: UserRepository 인터페이스에 findByEmail, findByToken 메소드 생성

--- BE/src/main/java/com/mj_eno/sidedish/domain/user/UserRepository.java
+++ BE/src/main/java/com/mj_eno/sidedish/domain/user/UserRepository.java
@@ -6,5 +6,7 @@ import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    Optional<User> findByUserId(String userId);
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByToken(String token);
 }
