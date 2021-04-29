1e39cc5d181b5f114c4ec17b8c018af91ec77b4d
jeong-inho <derosatam76@gmail.com>
2021-04-29 18:13:38 +0900

    Feat: User 클래스에 생성자와 update 메소드 생성

--- BE/src/main/java/com/mj_eno/sidedish/domain/user/User.java
+++ BE/src/main/java/com/mj_eno/sidedish/domain/user/User.java
@@ -1,5 +1,8 @@
 package com.mj_eno.sidedish.domain.user;
 
+import com.mj_eno.sidedish.web.dto.EmailDTO;
+import com.mj_eno.sidedish.web.dto.TokenDTO;
+import com.mj_eno.sidedish.web.dto.UserInfoDTO;
 import org.springframework.data.annotation.Id;
 
 public class User {
@@ -10,4 +13,18 @@ public class User {
     private String email;
     private String userId;
     private String token;
+
+    public User(UserInfoDTO userInfoDTO, EmailDTO emailDTO, TokenDTO tokenDTO) {
+        this.name = userInfoDTO.getName();
+        this.email = emailDTO.getEmail();
+        this.userId = userInfoDTO.getLogin();
+        this.token = tokenDTO.getAccess_token();
+    }
+
+    public void update(UserInfoDTO userInfoDTO, EmailDTO emailDTO, TokenDTO tokenDTO) {
+        this.name = userInfoDTO.getName();
+        this.email = emailDTO.getEmail();
+        this.userId = userInfoDTO.getLogin();
+        this.token = tokenDTO.getAccess_token();
+    }
 }
