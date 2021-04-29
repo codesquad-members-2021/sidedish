0e9eb2be65689096b55fc10766fea3e1cd259321
JinseoPark <rarebook92@naver.com>
2021-04-29 15:19:07 +0900

    feat : add functions data to DB and get from DB (#24)

--- iOS/BanchanCode/BanchanCode/Data/PersistentStorage/RealmManager.swift
+++ iOS/BanchanCode/BanchanCode/Data/PersistentStorage/RealmManager.swift
@@ -8,6 +8,35 @@
 import RealmSwift
 import Foundation
 
-class RealmManager {
+private protocol RealmOperations {
+    func add(dishesItem: [DishesItemViewModel], categoryName: String)
+    
+}
+
+class RealmManager: RealmOperations {
+    let realm = try! Realm()
+    
+    func add(dishesItem: [DishesItemViewModel], categoryName: String) { //dish 정보를 받아와서 dto?작업 후 데이터베이스에 저장한다.
+        dishesItem.forEach{ item in
+            //이미 primaryKey가 존재한다면 write를 할 시 에러가 난다.
+            if (realm.object(ofType: DishDB.self, forPrimaryKey: item.dish.id) == nil) {
+                
+                let dishDB = DishDB(id: item.dish.id, name: item.dish.name, contents: item.dish.description, imageURL: item.dish.imageURL, categoryName: categoryName)
+                
+                item.dish.prices.forEach {
+                    dishDB.prices.append($0)
+                }
+                item.dish.badges.forEach {
+                    dishDB.badges.append($0)
+                }
+                
+                try! realm.write {
+                    realm.add(dishDB)
+                }
+                //                        realmManager
+            }
+        }
+    }
+    
     
 }
