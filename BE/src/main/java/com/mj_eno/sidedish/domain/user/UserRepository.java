e9615a158bbe09bfe9a11721ec1644ba120f2067
jeong-inho <derosatam76@gmail.com>
2021-04-29 18:14:04 +0900

    Feat: UserRepository 인터페이스에 findByUserId 생성

--- BE/src/main/java/com/mj_eno/sidedish/domain/user/UserRepository.java
+++ BE/src/main/java/com/mj_eno/sidedish/domain/user/UserRepository.java
@@ -2,5 +2,9 @@ package com.mj_eno.sidedish.domain.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUserId(String userId);
 }
