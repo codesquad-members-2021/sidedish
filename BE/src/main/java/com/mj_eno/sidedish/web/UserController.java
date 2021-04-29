e87c930c5362276cbcbec90fc28ff1eace07c9da
jeong-inho <derosatam76@gmail.com>
2021-04-29 21:57:26 +0900

    Feat: UserController 클래스에 logout 메소드 생성

--- BE/src/main/java/com/mj_eno/sidedish/web/UserController.java
+++ BE/src/main/java/com/mj_eno/sidedish/web/UserController.java
@@ -4,9 +4,7 @@ import com.mj_eno.sidedish.service.UserService;
 import com.mj_eno.sidedish.web.dto.UserResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -18,9 +16,15 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public UserResponseDTO login(@RequestParam String code) {
         logger.info("로그인 요청");
         return userService.login(code);
+    }
+
+    @GetMapping("/logout")
+    public void logout(@RequestHeader String token) {
+        logger.info("로그아웃 요청");
+        userService.logout(token);
     }
 }
