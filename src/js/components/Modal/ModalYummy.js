8342627892543fac769a6fde0131095db0d86405
jjunyjjuny <jjuny546@gmail.com>
2021-04-30 14:59:53 +0900

    style : fix section acrticle width

--- src/js/components/Modal/ModalYummy.js
+++ src/js/components/Modal/ModalYummy.js
@@ -1,62 +1,70 @@
 import styled from "styled-components";
 import Card from "../common/Card";
-import carouselContext from "@jjunyjjuny/react-carousel";
+// import carouselContext from "@jjunyjjuny/react-carousel";
+import carouselContext from "./Carousel";
+
 import { useEffect, useState } from "react";
 const { Carousel, Controller } = carouselContext;
 
 const Yummy = ({ sibling, setModalData }) => {
-	const [cardList, setCardList] = useState(sibling);
-	useEffect(() => {
-		setCardList(() => sibling);
-	}, [sibling]);
-	console.log(cardList)
-	return (
-		<YummyWrapper>
-			<YummyHeader>
-				<Title>함께하면 더욱 맛있는 상품</Title>
-				<ControllerWrapper>
-					<Controller prev carouselId={4} />
-					<Controller next carouselId={4} />
-				</ControllerWrapper>
-			</YummyHeader>
-			<YummyCarousel>
-				<Carousel itemsPerPeice={5} customMode carouselId={4}>
-					{cardList.map((el) => {
-						console.log("in carousel ",cardList)
-						return <Card sibling={sibling} setModalData={setModalData} key={el.detail_hash} size="SMALL" data={el} />;
-					})}
-				</Carousel>
-			</YummyCarousel>
-		</YummyWrapper>
-	);
+  const [cardList, setCardList] = useState(sibling);
+  useEffect(() => {
+    setCardList(() => sibling);
+  }, [sibling]);
+  return (
+    <YummyWrapper>
+      <YummyHeader>
+        <Title>함께하면 더욱 맛있는 상품</Title>
+        <ControllerWrapper>
+          <Controller prev carouselId={4} />
+          <Controller next carouselId={4} />
+        </ControllerWrapper>
+      </YummyHeader>
+      <YummyCarousel>
+        <Carousel itemsPerPeice={5} customMode carouselId={4}>
+          {cardList.map((el) => {
+            return (
+              <Card
+                sibling={sibling}
+                setModalData={setModalData}
+                key={el.detail_hash}
+                size="SMALL"
+                data={el}
+              />
+            );
+          })}
+        </Carousel>
+      </YummyCarousel>
+    </YummyWrapper>
+  );
 };
 
 const YummyWrapper = styled.div`
-	width: 960px;
-	background: #f5f5f7;
-	border-radius: 0px 0px 5px 5px;
+  width: 960px;
+  background: #f5f5f7;
+  border-radius: 0px 0px 5px 5px;
 `;
 
 const YummyHeader = styled.div`
-	display: flex;
-	justify-content: space-between;
-	padding: 32px;
+  display: flex;
+  justify-content: space-between;
+  padding: 32px;
 `;
 const Title = styled.div`
-	font-family: Noto Sans KR;
-	font-style: normal;
-	font-weight: bold;
-	font-size: 18px;
-	line-height: 26px;
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 26px;
 `;
 const ControllerWrapper = styled.div`
-	display: flex;
+  display: flex;
 `;
 
 const YummyCarousel = styled.div`
-	display: flex;
-	/* justify-content:center; */
-    margin-left:10px;
+  display: flex;
+  /* justify-content:center; */
+  margin-left: 10px;
 `;
 
 export default Yummy;
--- src/js/components/Section/Article.js
+++ src/js/components/Section/Article.js
@@ -1,59 +1,75 @@
-import Carousel2 from "@jjunyjjuny/react-carousel";
+import Carousel2 from "./Carousel";
 import styled from "styled-components";
 import Card from "../common/Card";
 const { Carousel, Controller } = Carousel2;
 
 const Ariticle = ({ ariticle, index, setModalData, setModalOn }) => {
-	const title = ["모두가 좋아하는 든든한 메인요리", "정성이 담긴 뜨끈한 국물요리", "식탁을 풍성하게 하는 정갈한 밑반찬"];
-	return (
-		<AriticleWrapper>
-			<Title>{title[index]}</Title>
-			<CarouselWrapper>
-				<PrevButton>
-					<Controller prev carouselId={index + 1} />
-				</PrevButton>
-				<NextButton>
-					<Controller next carouselId={index + 1} />
-				</NextButton>
-				{ariticle.length > 0 && (
-					<Carousel itemsPerPeice={4} customMode autoFit carouselId={index + 1} gap={"0px"}>
-						{ariticle.map((el) => (
-							<Card sibling={ariticle} setModalData={setModalData} setModalOn={setModalOn} key={el.detail_hash} size="MEDIUM" data={el} />
-						))}
-					</Carousel>
-				)}
-			</CarouselWrapper>
-		</AriticleWrapper>
-	);
+  const title = [
+    "모두가 좋아하는 든든한 메인요리",
+    "정성이 담긴 뜨끈한 국물요리",
+    "식탁을 풍성하게 하는 정갈한 밑반찬",
+  ];
+  return (
+    <AriticleWrapper>
+      <Title>{title[index]}</Title>
+      <CarouselWrapper>
+        <PrevButton>
+          <Controller prev carouselId={index + 1} />
+        </PrevButton>
+        <NextButton>
+          <Controller next carouselId={index + 1} />
+        </NextButton>
+        {ariticle.length > 0 && (
+          <Carousel
+            itemsPerPeice={4}
+            customMode
+            carouselId={index + 1}
+            gap={"16px"}
+          >
+            {ariticle.map((el) => (
+              <Card
+                sibling={ariticle}
+                setModalData={setModalData}
+                setModalOn={setModalOn}
+                key={el.detail_hash}
+                size="MEDIUM"
+                data={el}
+              />
+            ))}
+          </Carousel>
+        )}
+      </CarouselWrapper>
+    </AriticleWrapper>
+  );
 };
 
 const AriticleWrapper = styled.div`
-	margin-top: 80px;
+  margin-top: 80px;
 `;
 const CarouselWrapper = styled.div`
-	display: flex;
-	position: relative;
-	width: 1280px;
+  width: 1280px;
+  position: relative;
+  display: flex;
 `;
 const PrevButton = styled.div`
-	position: absolute;
-	top: 230px;
-	left: -40px;
-	z-index: 2;
+  position: absolute;
+  top: 230px;
+  left: -40px;
+  z-index: 2;
 `;
 const NextButton = styled.div`
-	position: absolute;
-	top: 230px;
-	left: 1320px;
-	z-index: 2;
+  position: absolute;
+  top: 230px;
+  right: -40px;
+  z-index: 2;
 `;
 const Title = styled.div`
-	width: 1280px;
-	font-weight: bold;
-	font-size: 24px;
-	line-height: 35px;
-	margin: 0 auto;
-	margin-bottom: 32px;
+  width: 1280px;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 35px;
+  margin: 0 auto;
+  margin-bottom: 32px;
 `;
 
 export default Ariticle;
