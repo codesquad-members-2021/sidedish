ad6d179fffb1243eb1bfbb7abbc91477fc8bbf57
MJbae <gently.gogo@gmail.com>
2021-04-30 10:52:13 +0900

    fix: userController에 cors 허용에 따른 관련 에러 방지

--- BE/src/main/java/com/mj_eno/sidedish/web/UserController.java
+++ BE/src/main/java/com/mj_eno/sidedish/web/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
     public final UserService userService;
